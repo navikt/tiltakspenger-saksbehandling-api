a22a8664a3fda990543d23ce893e0646fc8c6c9f
John Andre Hestad <john.andre.hestad@nav.no>
2025-02-24 17:49:06 +0100

    Fjern deprecated perioder fra behandling-tabellen

---
+++ app/src/main/resources/db/migration/V40__behandling_fjern_deprecated_perioder.sql
@@ -0,0 +1,4 @@
+ALTER TABLE behandling DROP COLUMN fra_og_med;
+ALTER TABLE behandling DROP COLUMN til_og_med;
+ALTER TABLE behandling DROP COLUMN innvilgelsesperiode_fra_og_med;
+ALTER TABLE behandling DROP COLUMN innvilgelsesperiode_til_og_med;
