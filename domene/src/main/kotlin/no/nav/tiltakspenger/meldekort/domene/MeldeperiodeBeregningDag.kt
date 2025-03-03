91e911a33e1aa05dd4be54367f951065fa27061d
John Andre Hestad <john.andre.hestad@nav.no>
2025-03-03 17:05:02 +0100

    Fjern løst TODO

--- domene/src/main/kotlin/no/nav/tiltakspenger/meldekort/domene/MeldeperiodeBeregningDag.kt
+++ domene/src/main/kotlin/no/nav/tiltakspenger/meldekort/domene/MeldeperiodeBeregningDag.kt
@@ -111,7 +111,6 @@ sealed interface MeldeperiodeBeregningDag {
         data class IkkeDeltatt private constructor(
             override val meldekortId: MeldekortId,
             override val dato: LocalDate,
-            // TODO post-mvp: Siden vi bare støtter ett tiltak i MVP kan vi implisitt fylle ut denne. Dersom vi har flere tiltak, må bruker i så fall velge tiltaket han ikke har deltatt på og det er ikke sikkert det gir mening og vi tar bort feltet.
             override val tiltakstype: TiltakstypeSomGirRett,
             override val beregningsdag: Beregningsdag,
         ) : Utfylt {
