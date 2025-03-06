d5c61bf676924eeb0d3feaa9a9c4c0c8e1250a74
ramziabuqassim <ramzi.abu.qassim@nav.no>
2025-03-06 10:40:09 +0100

    lage ny søknad uten fnr for dev route

--- app/src/test/kotlin/no/nav/tiltakspenger/LocalDevRoutes.kt
+++ app/src/test/kotlin/no/nav/tiltakspenger/LocalDevRoutes.kt
@@ -6,7 +6,9 @@ import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import no.nav.tiltakspenger.libs.common.Fnr
 import no.nav.tiltakspenger.vedtak.context.ApplicationContext
-import no.nav.tiltakspenger.vedtak.routes.søknad.nySøknadPåEksisterendeSak
+import no.nav.tiltakspenger.vedtak.repository.felles.PeriodeDbJson
+import no.nav.tiltakspenger.vedtak.routes.søknad.nySakMedNySøknad
+import no.nav.tiltakspenger.vedtak.routes.søknad.nySøknadForFnr
 import no.nav.tiltakspenger.vedtak.routes.withBody
 
 internal const val DEV_ROUTE = "/dev"
@@ -14,20 +16,26 @@ internal const val DEV_ROUTE = "/dev"
 internal fun Route.localDevRoutes(applicationContext: ApplicationContext) {
     data class NySøknadBody(
         val fnr: String?,
+        val deltakelsesperiode: PeriodeDbJson?,
     )
 
     post("$DEV_ROUTE/soknad/ny") {
         call.withBody<NySøknadBody> {
             val fnr = it.fnr?.let { Fnr.tryFromString(it) }
             if (fnr == null) {
-                TODO()
-            } else {
-                nySøknadPåEksisterendeSak(
-                    fnr = fnr,
+                val saksnummer = nySakMedNySøknad(
+                    deltakelsesperiode = it.deltakelsesperiode?.toDomain(),
                     applicationContext = applicationContext,
                 )
+                call.respond(HttpStatusCode.OK, saksnummer.toString())
+            } else {
+                val saksnummer = nySøknadForFnr(
+                    fnr = fnr,
+                    applicationContext = applicationContext,
+                    deltakelsesperiode = it.deltakelsesperiode?.toDomain(),
+                )
+                call.respond(HttpStatusCode.OK, saksnummer.toString())
             }
-            call.respond(HttpStatusCode.OK, "Ok")
         }
     }
 }
--- app/src/test/kotlin/no/nav/tiltakspenger/vedtak/routes/søknad/MottaSøknadLokalt.kt
+++ app/src/test/kotlin/no/nav/tiltakspenger/vedtak/routes/søknad/MottaSøknadLokalt.kt
@@ -4,27 +4,62 @@ import kotlinx.coroutines.runBlocking
 import no.nav.tiltakspenger.felles.april
 import no.nav.tiltakspenger.libs.common.CorrelationId
 import no.nav.tiltakspenger.libs.common.Fnr
-import no.nav.tiltakspenger.libs.common.getOrFail
+import no.nav.tiltakspenger.libs.common.random
 import no.nav.tiltakspenger.libs.periodisering.Periode
 import no.nav.tiltakspenger.objectmothers.ObjectMother
+import no.nav.tiltakspenger.saksbehandling.domene.sak.Saksnummer
 import no.nav.tiltakspenger.vedtak.context.ApplicationContext
 
-fun nySøknadPåEksisterendeSak(
+fun nySøknadForFnr(
     fnr: Fnr,
-    deltakelsesperiode: Periode = Periode(1.april(2025), 10.april(2025)),
+    deltakelsesperiode: Periode? = null,
     applicationContext: ApplicationContext,
-) {
-    runBlocking {
-        val sak = applicationContext.sakContext.sakService.hentForFnr(fnr, ObjectMother.saksbehandler(), CorrelationId.generate()).getOrFail()
+): Saksnummer {
+    val periode = deltakelsesperiode ?: Periode(1.april(2025), 10.april(2025))
+
+    return runBlocking {
+        val sak = applicationContext.sakContext.sakService.hentEllerOpprettSak(
+            fnr,
+            ObjectMother.systembrukerLageHendelser(),
+            CorrelationId.generate(),
+        )
         val søknad = ObjectMother.nySøknad(
             fnr = fnr,
             sakId = sak.id,
             saksnummer = sak.saksnummer,
-            periode = deltakelsesperiode,
+            periode = periode,
         )
         applicationContext.søknadContext.søknadService.nySøknad(
             søknad = søknad,
             systembruker = ObjectMother.systembrukerLageHendelser(),
         )
+
+        sak.saksnummer
+    }
+}
+
+fun nySakMedNySøknad(
+    deltakelsesperiode: Periode? = null,
+    applicationContext: ApplicationContext,
+): Saksnummer {
+    val periode = deltakelsesperiode ?: Periode(1.april(2025), 10.april(2025))
+
+    return runBlocking {
+        val sak = applicationContext.sakContext.sakService.hentEllerOpprettSak(
+            Fnr.random(),
+            ObjectMother.systembrukerLageHendelser(),
+            CorrelationId.generate(),
+        )
+        val søknad = ObjectMother.nySøknad(
+            fnr = sak.fnr,
+            sakId = sak.id,
+            saksnummer = sak.saksnummer,
+            periode = periode,
+        )
+        applicationContext.søknadContext.søknadService.nySøknad(
+            søknad = søknad,
+            systembruker = ObjectMother.systembrukerLageHendelser(),
+        )
+        sak.saksnummer
     }
 }
