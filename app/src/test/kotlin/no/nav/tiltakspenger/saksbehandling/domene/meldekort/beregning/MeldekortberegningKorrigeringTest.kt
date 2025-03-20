d00b2c3482a938f03107b5851e56a524262033db
Anders B Nomerstad <anders@nomerstad.com>
2025-03-20 15:47:00 +0100

    Tester beløp ved korrigering

--- app/src/test/kotlin/no/nav/tiltakspenger/saksbehandling/domene/meldekort/beregning/MeldekortberegningKorrigeringTest.kt
+++ app/src/test/kotlin/no/nav/tiltakspenger/saksbehandling/domene/meldekort/beregning/MeldekortberegningKorrigeringTest.kt
@@ -1,12 +1,11 @@
 package no.nav.tiltakspenger.saksbehandling.domene.meldekort.beregning
 
-import arrow.core.NonEmptyList
 import arrow.core.toNonEmptyListOrNull
 import io.kotest.matchers.shouldBe
 import kotlinx.datetime.DayOfWeek
 import no.nav.tiltakspenger.libs.common.CorrelationId
-import no.nav.tiltakspenger.libs.common.MeldekortId
 import no.nav.tiltakspenger.libs.common.SakId
+import no.nav.tiltakspenger.libs.common.getOrFail
 import no.nav.tiltakspenger.libs.periodisering.Periode
 import no.nav.tiltakspenger.libs.periodisering.PeriodeMedVerdi
 import no.nav.tiltakspenger.libs.periodisering.Periodisering
@@ -14,16 +13,17 @@ import no.nav.tiltakspenger.libs.tiltak.TiltakstypeSomGirRett
 import no.nav.tiltakspenger.saksbehandling.barnetillegg.AntallBarn
 import no.nav.tiltakspenger.saksbehandling.meldekort.domene.MeldekortBehandlingType
 import no.nav.tiltakspenger.saksbehandling.meldekort.domene.MeldekortBehandlinger
-import no.nav.tiltakspenger.saksbehandling.meldekort.domene.MeldeperiodeBeregningDag
 import no.nav.tiltakspenger.saksbehandling.meldekort.domene.SendMeldekortTilBeslutningKommando
-import no.nav.tiltakspenger.saksbehandling.meldekort.domene.beregn
 import no.nav.tiltakspenger.saksbehandling.objectmothers.ObjectMother
+import no.nav.tiltakspenger.saksbehandling.utbetaling.domene.Satser
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 internal class MeldekortberegningKorrigeringTest {
     private val førsteDag = LocalDate.of(2025, 1, 6)
     private val førstePeriode = Periode(fraOgMed = førsteDag, tilOgMed = førsteDag.plusDays(13))
+
+    private val sats2025 = Satser.sats(førsteDag).sats
 
     private val deltatt10Dager =
         (0L..13L).map { index ->
@@ -39,68 +39,86 @@ internal class MeldekortberegningKorrigeringTest {
             )
         }.toNonEmptyListOrNull()!!
 
-    private fun forventetBeregning(
-        dager: NonEmptyList<SendMeldekortTilBeslutningKommando.Dager.Dag>,
-        meldekortId: MeldekortId,
-        tiltakstype: TiltakstypeSomGirRett,
-        antallBarn: AntallBarn,
-    ) = dager.map {
-        when (it.status) {
-            SendMeldekortTilBeslutningKommando.Status.DELTATT_UTEN_LØNN_I_TILTAKET -> MeldeperiodeBeregningDag.Utfylt.Deltatt.DeltattUtenLønnITiltaket.create(
-                meldekortId = meldekortId,
-                dato = it.dag,
-                antallBarn = antallBarn,
-                tiltakstype = tiltakstype,
-            )
+    private val deltatt5DagerUgyldigFravær5Dager =
+        (0L..13L).map { index ->
+            val dag = førsteDag.plusDays(index)
+            val status = when (dag.dayOfWeek) {
+                DayOfWeek.SATURDAY, DayOfWeek.SUNDAY -> SendMeldekortTilBeslutningKommando.Status.IKKE_DELTATT
+                else -> if (index > 6) {
+                    SendMeldekortTilBeslutningKommando.Status.DELTATT_UTEN_LØNN_I_TILTAKET
+                } else {
+                    SendMeldekortTilBeslutningKommando.Status.FRAVÆR_VELFERD_IKKE_GODKJENT_AV_NAV
+                }
+            }
 
-            else -> MeldeperiodeBeregningDag.Utfylt.IkkeDeltatt.create(
-                meldekortId = meldekortId,
-                dato = it.dag,
-                antallBarn = antallBarn,
-                tiltakstype = tiltakstype,
+            SendMeldekortTilBeslutningKommando.Dager.Dag(
+                dag = dag,
+                status = status,
             )
-        }
-    }
+        }.toNonEmptyListOrNull()!!
 
     @Test
     fun `Skal beregne for korrigering`() {
         val sakId = SakId.random()
 
-        val meldekortBehandlet1 = ObjectMother.meldekortBehandlet(sakId = sakId, periode = førstePeriode)
-        val meldekortBehandlet2 = ObjectMother.meldekortBehandlet(sakId = sakId, periode = førstePeriode.plus14Dager())
+        val barnetilleggsPerioder = Periodisering<AntallBarn>()
+        val tiltakstypePerioder = Periodisering(
+            PeriodeMedVerdi(
+                periode = Periode(fraOgMed = førsteDag, tilOgMed = førsteDag.plusDays(100)),
+                verdi = TiltakstypeSomGirRett.JOBBKLUBB,
+            ),
+        )
 
-        val meldekortKorrigert = ObjectMother.meldekortUnderBehandling(
+        val meldekortFørstegangsBehandling = ObjectMother.meldekortUnderBehandling(
+            sakId = sakId,
+            periode = førstePeriode,
+            type = MeldekortBehandlingType.FØRSTE_BEHANDLING,
+        )
+
+        val meldekortBehandlingerMedFørsteUnderBehandling = MeldekortBehandlinger(
+            listOf(meldekortFørstegangsBehandling),
+        )
+
+        val førsteBehandlingKommando = SendMeldekortTilBeslutningKommando(
+            sakId = sakId,
+            meldekortId = meldekortFørstegangsBehandling.id,
+            saksbehandler = ObjectMother.saksbehandler(navIdent = meldekortFørstegangsBehandling.saksbehandler),
+            correlationId = CorrelationId.generate(),
+            dager = SendMeldekortTilBeslutningKommando.Dager(deltatt10Dager),
+        )
+
+        val meldekortFørstegangsBehandlet = meldekortBehandlingerMedFørsteUnderBehandling.sendTilBeslutter(
+            førsteBehandlingKommando,
+            barnetilleggsPerioder,
+            tiltakstypePerioder,
+        ).getOrFail().second
+
+        meldekortFørstegangsBehandlet.beløpTotal shouldBe sats2025 * 10
+
+        val meldekortKorrigering = ObjectMother.meldekortUnderBehandling(
             sakId = sakId,
             periode = førstePeriode,
             type = MeldekortBehandlingType.KORRIGERING,
         )
 
-        val kommando = SendMeldekortTilBeslutningKommando(
+        val korrigeringKommando = SendMeldekortTilBeslutningKommando(
             sakId = sakId,
-            meldekortId = meldekortKorrigert.id,
-            saksbehandler = ObjectMother.saksbehandler(navIdent = meldekortKorrigert.saksbehandler),
+            meldekortId = meldekortKorrigering.id,
+            saksbehandler = ObjectMother.saksbehandler(navIdent = meldekortKorrigering.saksbehandler),
             correlationId = CorrelationId.generate(),
-            dager = SendMeldekortTilBeslutningKommando.Dager(deltatt10Dager),
+            dager = SendMeldekortTilBeslutningKommando.Dager(deltatt5DagerUgyldigFravær5Dager),
         )
 
-        val beregning = kommando.beregn(
-            barnetilleggsPerioder = Periodisering(),
-            tiltakstypePerioder = Periodisering(
-                PeriodeMedVerdi(
-                    periode = Periode(fraOgMed = førsteDag, tilOgMed = førsteDag.plusDays(100)),
-                    verdi = TiltakstypeSomGirRett.JOBBKLUBB,
-                ),
-            ),
-            eksisterendeMeldekortBehandlinger = MeldekortBehandlinger(
-                listOf(meldekortBehandlet1, meldekortBehandlet2, meldekortKorrigert),
-            ),
+        val meldekortBehandlingerMedKorrigeringUnderBehandling = MeldekortBehandlinger(
+            listOf(meldekortFørstegangsBehandlet, meldekortKorrigering),
         )
 
-        beregning shouldBe forventetBeregning(
-            deltatt10Dager,
-            meldekortKorrigert.id,
-            TiltakstypeSomGirRett.JOBBKLUBB,
-            AntallBarn(0),
-        )
+        val meldekortKorrigeringBehandlet = meldekortBehandlingerMedKorrigeringUnderBehandling.sendTilBeslutter(
+            korrigeringKommando,
+            barnetilleggsPerioder,
+            tiltakstypePerioder,
+        ).getOrFail().second
+
+        meldekortKorrigeringBehandlet.beløpTotal shouldBe sats2025 * 5
     }
 }
