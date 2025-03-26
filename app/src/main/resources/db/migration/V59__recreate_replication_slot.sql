149846ce0070c0accfdb2eeac6c665ac8c025e13
Tia Firing <tia.firing@nav.no>
2025-03-26 16:24:12 +0100

    1414 forsøker å sette opp rettigheter, replication og publication på nytt

--- app/src/main/resources/db/migration/V59__recreate_replication_slot.sql
+++ app/src/main/resources/db/migration/V59__recreate_replication_slot.sql
@@ -1,8 +1,50 @@
 DO
 $$
-BEGIN
-    IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'tpts_ds') THEN
-        PERFORM PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
-    END IF;
-END
-$$ LANGUAGE 'plpgsql';
+    BEGIN
+        IF EXISTS
+                (SELECT 1 from pg_roles where rolname = 'tiltakspenger-saksbehandling-api')
+        THEN
+            ALTER USER "tiltakspenger-saksbehandling-api" WITH REPLICATION;
+        END IF;
+    END
+$$;
+END;
+
+DO
+$$
+    BEGIN
+        IF EXISTS
+                (SELECT 1 from pg_roles where rolname = 'tpts_ds')
+        THEN
+            ALTER USER "tpts_ds" WITH REPLICATION;
+            ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO "tpts_ds";
+            GRANT USAGE ON SCHEMA public TO "tpts_ds";
+            GRANT SELECT ON ALL TABLES IN SCHEMA public TO "tpts_ds";
+        END IF;
+    END
+$$;
+END;
+
+DO
+$$
+    BEGIN
+        if not exists
+                (select 1 from pg_publication where pubname = 'ds_publication')
+        then
+            CREATE PUBLICATION ds_publication for ALL TABLES;
+        end if;
+    end;
+$$;
+END;
+
+DO
+$$
+    BEGIN
+        if not exists
+            (select 1 from pg_replication_slots where slot_name = 'ds_replication')
+        then
+            PERFORM PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
+        end if;
+    end;
+$$;
+END;
