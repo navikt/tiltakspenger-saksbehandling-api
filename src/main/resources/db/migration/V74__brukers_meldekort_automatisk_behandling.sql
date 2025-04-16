24e34bf13c36a41b466adee667aef148286865e2
Anders B Nomerstad <anders@nomerstad.com>
2025-04-16 10:25:38 +0200

    Fjerner index

--- src/main/resources/db/migration/V74__brukers_meldekort_automatisk_behandling.sql
+++ src/main/resources/db/migration/V74__brukers_meldekort_automatisk_behandling.sql
@@ -5,5 +5,3 @@ ALTER TABLE meldekort_bruker
 
 ALTER TABLE meldekortbehandling
     ADD COLUMN IF NOT EXISTS brukers_meldekort_id varchar NULL;
-
-CREATE INDEX idx_meldekortbehandling_brukers_meldekort_id ON meldekortbehandling (brukers_meldekort_id);
