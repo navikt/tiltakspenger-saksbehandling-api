4ff7e1a4cabc98af5b7facb96c15c7a98fcc32ad
Tia Firing <tia.firing@nav.no>
2025-05-02 13:54:23 +0200

    1471 retter type for ny kolonne

--- src/main/resources/db/migration/V80__statistikk_utbetaling_add_columns.sql
+++ src/main/resources/db/migration/V80__statistikk_utbetaling_add_columns.sql
@@ -1,4 +1,4 @@
-ALTER TABLE statistikk_utbetaling ADD COLUMN IF NOT EXISTS vedtak_id varchar;
+ALTER TABLE statistikk_utbetaling ADD COLUMN IF NOT EXISTS vedtak_id jsonb;
 ALTER TABLE statistikk_utbetaling ADD COLUMN IF NOT EXISTS opprettet TIMESTAMPTZ;
 ALTER TABLE statistikk_utbetaling ADD COLUMN IF NOT EXISTS sist_endret TIMESTAMPTZ;
 
