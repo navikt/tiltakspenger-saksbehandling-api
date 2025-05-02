f3e60faa22d22775ee6db66da31d43c8753dec91
Tia Firing <tia.firing@nav.no>
2025-05-02 10:37:22 +0200

    wip: nye kolonner som skal deles med dvh

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/service/statistikk/stønad/StatistikkUtbetalingDTO.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/service/statistikk/stønad/StatistikkUtbetalingDTO.kt
@@ -1,18 +1,23 @@
 package no.nav.tiltakspenger.saksbehandling.behandling.service.statistikk.stønad
 
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 data class StatistikkUtbetalingDTO(
+    // id for utbetalingsvedtaket
     val id: String,
     val sakId: String,
     val saksnummer: String,
     val ordinærBeløp: Int,
     val barnetilleggBeløp: Int,
     val totalBeløp: Int,
-    val årsak: String,
     val posteringDato: LocalDate,
     val gyldigFraDatoPostering: LocalDate,
     val gyldigTilDatoPostering: LocalDate,
     // id-en som vi sender til helved
     val utbetalingId: String,
+    // vedtaket som er bakgrunnen for utbetalingen
+    val vedtakId: String?,
+    val opprettet: LocalDateTime?,
+    val sistEndret: LocalDateTime?,
 )
