ae7df4e19ec367983b23cb4a97c5870128968d8a
ramziabuqassim <ramzi.abu.qassim@nav.no>
2025-05-13 11:46:55 +0200

    Legger til ValgtHjemmelForStansDTO for å erstatte ValgtHjemmelHarIkkeRettighet og oppdaterer relaterte metoder

---
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/infra/route/brev/ValgtHjemmelForStansDTO.kt
@@ -0,0 +1,28 @@
+package no.nav.tiltakspenger.saksbehandling.behandling.infra.route.brev
+
+import no.nav.tiltakspenger.saksbehandling.behandling.domene.ValgtHjemmelForStans
+
+enum class ValgtHjemmelForStansDTO {
+    DeltarIkkePåArbeidsmarkedstiltak,
+    Alder,
+    Livsoppholdytelser,
+    Kvalifiseringsprogrammet,
+    Introduksjonsprogrammet,
+    LønnFraTiltaksarrangør,
+    LønnFraAndre,
+    Institusjonsopphold,
+    ;
+
+    fun toDomain(): ValgtHjemmelForStans = when (this) {
+        DeltarIkkePåArbeidsmarkedstiltak -> ValgtHjemmelForStans.DeltarIkkePåArbeidsmarkedstiltak
+        Alder -> ValgtHjemmelForStans.Alder
+        Livsoppholdytelser -> ValgtHjemmelForStans.Livsoppholdytelser
+        Kvalifiseringsprogrammet -> ValgtHjemmelForStans.Kvalifiseringsprogrammet
+        Introduksjonsprogrammet -> ValgtHjemmelForStans.Introduksjonsprogrammet
+        LønnFraTiltaksarrangør -> ValgtHjemmelForStans.LønnFraTiltaksarrangør
+        LønnFraAndre -> ValgtHjemmelForStans.LønnFraAndre
+        Institusjonsopphold -> ValgtHjemmelForStans.Institusjonsopphold
+    }
+}
+
+fun List<ValgtHjemmelForStansDTO>.toDomain(): List<ValgtHjemmelForStans> = this.map { it.toDomain() }
