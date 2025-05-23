346174352bc08cbccd178676c7954a86bfa19e90
Henrik Solum <henrik.solum@nav.no>
2025-05-23 17:08:13 +0200

    1426 WIP Gjenåpne behandling

---
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/service/behandling/GjenåpneBehandlingService.kt
@@ -0,0 +1,48 @@
+package no.nav.tiltakspenger.saksbehandling.behandling.service.behandling
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import no.nav.tiltakspenger.libs.common.BehandlingId
+import no.nav.tiltakspenger.libs.common.CorrelationId
+import no.nav.tiltakspenger.libs.common.Saksbehandler
+import no.nav.tiltakspenger.libs.persistering.domene.SessionFactory
+import no.nav.tiltakspenger.libs.personklient.pdl.TilgangsstyringService
+import no.nav.tiltakspenger.saksbehandling.behandling.domene.Behandling
+import no.nav.tiltakspenger.saksbehandling.behandling.ports.BehandlingRepo
+import no.nav.tiltakspenger.saksbehandling.behandling.ports.StatistikkSakRepo
+import no.nav.tiltakspenger.saksbehandling.felles.exceptions.krevSaksbehandlerEllerBeslutterRolle
+import no.nav.tiltakspenger.saksbehandling.felles.exceptions.krevTilgangTilPerson
+import no.nav.tiltakspenger.saksbehandling.statistikk.behandling.StatistikkSakService
+
+class GjenåpneBehandlingService(
+    private val tilgangsstyringService: TilgangsstyringService,
+    private val behandlingRepo: BehandlingRepo,
+    private val statistikkSakService: StatistikkSakService,
+    private val statistikkSakRepo: StatistikkSakRepo,
+    private val sessionFactory: SessionFactory,
+) {
+    val logger = KotlinLogging.logger { }
+
+    suspend fun gjenåpneBehandling(
+        behandlingId: BehandlingId,
+        saksbehandler: Saksbehandler,
+        correlationId: CorrelationId,
+    ): Behandling {
+        krevSaksbehandlerEllerBeslutterRolle(saksbehandler)
+
+        val behandling = behandlingRepo.hent(behandlingId)
+        tilgangsstyringService.krevTilgangTilPerson(saksbehandler, behandling.fnr, correlationId)
+
+        return behandling.gjenåpneBehandling(saksbehandler).also {
+            val statistikk = statistikkSakService.genererStatistikkForOppdatertSaksbehandlerEllerBeslutter(it)
+            sessionFactory.withTransactionContext { tx ->
+                behandlingRepo.leggTilbakeBehandlingSaksbehandler(
+                    it.id,
+                    saksbehandler,
+                    it.status,
+                    tx,
+                )
+                statistikkSakRepo.lagre(statistikk, tx)
+            }
+        }
+    }
+}
