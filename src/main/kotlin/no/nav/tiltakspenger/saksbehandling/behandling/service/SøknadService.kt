cf3b985e058ccd60348fd383dc6c35b4f29c1e9d
John Andre Hestad <john.andre.hestad@nav.no>
2025-05-23 16:44:47 +0200

    Fjern interfacet til SøknadService
    Trenger ikke interface på servicene

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/service/SøknadService.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/service/SøknadService.kt
@@ -1,18 +1,41 @@
 package no.nav.tiltakspenger.saksbehandling.behandling.service
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.tiltakspenger.libs.common.SakId
 import no.nav.tiltakspenger.libs.common.SøknadId
 import no.nav.tiltakspenger.libs.persistering.domene.TransactionContext
+import no.nav.tiltakspenger.saksbehandling.behandling.ports.OppgaveGateway
+import no.nav.tiltakspenger.saksbehandling.behandling.ports.Oppgavebehov
+import no.nav.tiltakspenger.saksbehandling.behandling.ports.SøknadRepo
 import no.nav.tiltakspenger.saksbehandling.felles.Systembruker
+import no.nav.tiltakspenger.saksbehandling.felles.exceptions.krevLageHendelserRollen
+import no.nav.tiltakspenger.saksbehandling.journalføring.JournalpostId
 import no.nav.tiltakspenger.saksbehandling.søknad.Søknad
 
-interface SøknadService {
+class SøknadServiceImpl(
+    private val søknadRepo: SøknadRepo,
+    private val oppgaveGateway: OppgaveGateway,
+) : SøknadService {
+    private val log = KotlinLogging.logger {}
+
     /** Skal i førsteomgang kun brukes til digitale søknader. Dersom en saksbehandler skal registere en papirsøknad må vi ha en egen funksjon som sjekker tilgang.*/
-    suspend fun nySøknad(søknad: Søknad, systembruker: Systembruker)
+    override suspend fun nySøknad(søknad: Søknad, systembruker: Systembruker) {
+        krevLageHendelserRollen(systembruker)
+        val oppgaveId =
+            oppgaveGateway.opprettOppgave(søknad.fnr, JournalpostId(søknad.journalpostId), Oppgavebehov.NY_SOKNAD)
+        log.info { "Opprettet oppgave med id $oppgaveId for søknad med id ${søknad.id}" }
+        søknadRepo.lagre(søknad.copy(oppgaveId = oppgaveId))
+    }
 
-    fun hentSøknad(søknadId: SøknadId): Søknad
+    override fun hentSøknad(søknadId: SøknadId): Søknad {
+        return søknadRepo.hentForSøknadId(søknadId)!!
+    }
 
-    fun hentSakIdForSoknad(søknadId: SøknadId): SakId
+    override fun hentSakIdForSoknad(søknadId: SøknadId): SakId {
+        return søknadRepo.hentSakIdForSoknad(søknadId)!!
+    }
 
-    fun lagreAvbruttSøknad(søknad: Søknad, tx: TransactionContext)
+    override fun lagreAvbruttSøknad(søknad: Søknad, tx: TransactionContext) {
+        søknadRepo.lagreAvbruttSøknad(søknad, tx)
+    }
 }
