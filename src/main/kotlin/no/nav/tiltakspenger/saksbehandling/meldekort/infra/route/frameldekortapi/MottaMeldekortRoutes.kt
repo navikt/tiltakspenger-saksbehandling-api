a6bdb5d234795e8bb929cfac275e34d4a890f976
Tia Firing <tia.firing@nav.no>
2025-05-30 13:22:12 +0200

    må ha tilgangskontroll på motta meldekort-api

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/meldekort/infra/route/frameldekortapi/MottaMeldekortRoutes.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/meldekort/infra/route/frameldekortapi/MottaMeldekortRoutes.kt
@@ -5,11 +5,15 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
+import no.nav.tiltakspenger.libs.auth.core.TokenService
+import no.nav.tiltakspenger.libs.auth.ktor.withSystembruker
 import no.nav.tiltakspenger.libs.common.MeldekortId
 import no.nav.tiltakspenger.libs.common.SakId
 import no.nav.tiltakspenger.libs.ktor.common.withBody
 import no.nav.tiltakspenger.libs.meldekort.BrukerutfyltMeldekortDTO
 import no.nav.tiltakspenger.libs.meldekort.MeldeperiodeId
+import no.nav.tiltakspenger.saksbehandling.felles.Systembruker
+import no.nav.tiltakspenger.saksbehandling.felles.exceptions.krevLageHendelserRollen
 import no.nav.tiltakspenger.saksbehandling.journalføring.JournalpostId
 import no.nav.tiltakspenger.saksbehandling.meldekort.domene.BrukersMeldekort.BrukersMeldekortDag
 import no.nav.tiltakspenger.saksbehandling.meldekort.domene.InnmeldtStatus
@@ -21,32 +25,36 @@ private const val PATH = "/meldekort/motta"
 
 internal fun Route.mottaMeldekortRoutes(
     mottaBrukerutfyltMeldekortService: MottaBrukerutfyltMeldekortService,
+    tokenService: TokenService,
 ) {
     val logger = KotlinLogging.logger { }
 
     post(PATH) {
         logger.debug { "Mottatt post-request på $PATH" }
-        call.withBody<BrukerutfyltMeldekortDTO> { meldekort ->
-            logger.info { "Mottatt meldekort fra bruker: ${meldekort.id} - meldeperiode: ${meldekort.meldeperiodeId}" }
+        call.withSystembruker(tokenService = tokenService) { systembruker: Systembruker ->
+            call.withBody<BrukerutfyltMeldekortDTO> { meldekort ->
+                logger.info { "Mottatt meldekort fra bruker: ${meldekort.id} - meldeperiode: ${meldekort.meldeperiodeId}" }
 
-            mottaBrukerutfyltMeldekortService.mottaBrukerutfyltMeldekort(meldekort.toDomain()).onRight {
-                call.respond(HttpStatusCode.OK)
-            }.onLeft {
-                when (it) {
-                    is KanIkkeLagreBrukersMeldekort.AlleredeLagretUtenDiff -> call.respond(
-                        status = HttpStatusCode.OK,
-                        message = "Meldekort med id ${meldekort.id} var allerede lagret med samme data",
-                    )
+                krevLageHendelserRollen(systembruker)
+                mottaBrukerutfyltMeldekortService.mottaBrukerutfyltMeldekort(meldekort.toDomain()).onRight {
+                    call.respond(HttpStatusCode.OK)
+                }.onLeft {
+                    when (it) {
+                        is KanIkkeLagreBrukersMeldekort.AlleredeLagretUtenDiff -> call.respond(
+                            status = HttpStatusCode.OK,
+                            message = "Meldekort med id ${meldekort.id} var allerede lagret med samme data",
+                        )
 
-                    is KanIkkeLagreBrukersMeldekort.AlleredeLagretMedDiff -> call.respond(
-                        status = HttpStatusCode.Conflict,
-                        message = "Meldekort med id ${meldekort.id} var allerede lagret med andre data!",
-                    )
+                        is KanIkkeLagreBrukersMeldekort.AlleredeLagretMedDiff -> call.respond(
+                            status = HttpStatusCode.Conflict,
+                            message = "Meldekort med id ${meldekort.id} var allerede lagret med andre data!",
+                        )
 
-                    is KanIkkeLagreBrukersMeldekort.UkjentFeil -> call.respond(
-                        status = HttpStatusCode.InternalServerError,
-                        message = "Kunne ikke lagre brukers meldekort - Ukjent feil",
-                    )
+                        is KanIkkeLagreBrukersMeldekort.UkjentFeil -> call.respond(
+                            status = HttpStatusCode.InternalServerError,
+                            message = "Kunne ikke lagre brukers meldekort - Ukjent feil",
+                        )
+                    }
                 }
             }
         }
