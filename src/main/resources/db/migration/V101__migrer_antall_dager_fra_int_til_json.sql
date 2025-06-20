749a3096b4a58af760831bcf06cb5424e2a9b556
John Andre Hestad <john.andre.hestad@nav.no>
2025-06-20 11:02:06 +0200

    Gjør migreringsskriptet litt mer failsafe

--- src/main/resources/db/migration/V101__migrer_antall_dager_fra_int_til_json.sql
+++ src/main/resources/db/migration/V101__migrer_antall_dager_fra_int_til_json.sql
@@ -1,8 +1,10 @@
+UPDATE behandling SET antall_dager_per_meldeperiode_json = null where 1=1;
+
 UPDATE behandling
 SET antall_dager_per_meldeperiode_json =
         jsonb_build_array(
                 jsonb_build_object(
-                        'antallDagerPerMeldeperiode', antall_dager_per_meldeperiode,
+                        'antallDagerPerMeldeperiode', antall_dager_per_meldeperiode::int,
                         'periode', jsonb_build_object(
                                 'fra_og_med', virkningsperiode_fra_og_med,
                                 'til_og_med', virkningsperiode_til_og_med
