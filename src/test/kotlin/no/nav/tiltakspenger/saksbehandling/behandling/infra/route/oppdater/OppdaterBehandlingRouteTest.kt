bdc915c637df1d7cd3a4f10421fc8646d1d2b1e1
Anders B Nomerstad <anders@nomerstad.com>
2025-08-05 12:28:52 +0200

    Tester for oppdatering av alle behandling-resultater

--- src/test/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/infra/route/oppdater/OppdaterBehandlingRouteTest.kt
+++ src/test/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/infra/route/oppdater/OppdaterBehandlingRouteTest.kt
@@ -1,21 +1,35 @@
 package no.nav.tiltakspenger.saksbehandling.behandling.infra.route.oppdater
 
+import arrow.core.nonEmptySetOf
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 import io.ktor.server.routing.routing
 import io.ktor.server.testing.testApplication
+import no.nav.tiltakspenger.libs.dato.april
 import no.nav.tiltakspenger.libs.periodisering.SammenhengendePeriodisering
 import no.nav.tiltakspenger.libs.periodisering.toDTO
 import no.nav.tiltakspenger.saksbehandling.barnetillegg.AntallBarn
 import no.nav.tiltakspenger.saksbehandling.behandling.domene.AntallDagerForMeldeperiode
+import no.nav.tiltakspenger.saksbehandling.behandling.domene.Avslagsgrunnlag
 import no.nav.tiltakspenger.saksbehandling.behandling.domene.BegrunnelseVilkårsvurdering
 import no.nav.tiltakspenger.saksbehandling.behandling.domene.MAKS_DAGER_MED_TILTAKSPENGER_FOR_PERIODE
+import no.nav.tiltakspenger.saksbehandling.behandling.domene.RevurderingResultat
+import no.nav.tiltakspenger.saksbehandling.behandling.domene.RevurderingResultat.Stans
+import no.nav.tiltakspenger.saksbehandling.behandling.domene.SøknadsbehandlingResultat
+import no.nav.tiltakspenger.saksbehandling.behandling.domene.SøknadsbehandlingResultat.Avslag
+import no.nav.tiltakspenger.saksbehandling.behandling.domene.ValgtHjemmelForStans
 import no.nav.tiltakspenger.saksbehandling.behandling.infra.route.barnetillegg.toBarnetilleggDTO
+import no.nav.tiltakspenger.saksbehandling.behandling.infra.route.dto.OppdaterRevurderingDTO
 import no.nav.tiltakspenger.saksbehandling.behandling.infra.route.dto.OppdaterSøknadsbehandlingDTO
+import no.nav.tiltakspenger.saksbehandling.behandling.infra.route.dto.ValgtHjemmelForAvslagDTO
+import no.nav.tiltakspenger.saksbehandling.behandling.infra.route.dto.ValgtHjemmelForStansDTO
 import no.nav.tiltakspenger.saksbehandling.common.TestApplicationContext
 import no.nav.tiltakspenger.saksbehandling.infra.route.routes
 import no.nav.tiltakspenger.saksbehandling.infra.setup.jacksonSerialization
 import no.nav.tiltakspenger.saksbehandling.objectmothers.ObjectMother.barnetillegg
 import no.nav.tiltakspenger.saksbehandling.routes.RouteBehandlingBuilder.opprettSøknadsbehandlingUnderBehandling
+import no.nav.tiltakspenger.saksbehandling.routes.RouteBehandlingBuilder.startRevurderingInnvilgelse
+import no.nav.tiltakspenger.saksbehandling.routes.RouteBehandlingBuilder.startRevurderingStans
 import no.nav.tiltakspenger.saksbehandling.tiltaksdeltagelse.infra.route.TiltaksdeltakelsePeriodeDTO
 import no.nav.tiltakspenger.saksbehandling.tiltaksdeltagelse.infra.route.toDTO
 import org.junit.jupiter.api.Test
@@ -68,11 +82,142 @@ class OppdaterBehandlingRouteTest {
 
             val oppdatertBehandling = tac.behandlingContext.behandlingRepo.hent(behandling.id)
 
+            oppdatertBehandling.resultat.shouldBeInstanceOf<SøknadsbehandlingResultat.Innvilgelse>()
             oppdatertBehandling.fritekstTilVedtaksbrev!!.verdi shouldBe "ny brevtekst"
             oppdatertBehandling.begrunnelseVilkårsvurdering!!.verdi shouldBe "ny begrunnelse"
             oppdatertBehandling.virkningsperiode shouldBe nyInnvilgelsesperiode
             oppdatertBehandling.barnetillegg shouldBe barnetillegg
             oppdatertBehandling.antallDagerPerMeldeperiode shouldBe antallDager
+        }
+    }
+
+    @Test
+    fun `kan oppdatere avslått søknadsbehandling`() {
+        val tac = TestApplicationContext()
+
+        testApplication {
+            application {
+                jacksonSerialization()
+                routing { routes(tac) }
+            }
+
+            val (sak, _, behandling) = opprettSøknadsbehandlingUnderBehandling(tac)
+
+            val tiltaksdeltagelse = behandling.saksopplysninger.tiltaksdeltagelse.first()
+
+            oppdaterBehandling(
+                tac = tac,
+                sakId = sak.id,
+                behandlingId = behandling.id,
+                oppdaterBehandlingDTO = OppdaterSøknadsbehandlingDTO.Avslag(
+                    fritekstTilVedtaksbrev = "ny brevtekst",
+                    begrunnelseVilkårsvurdering = "ny begrunnelse",
+                    valgteTiltaksdeltakelser = listOf(
+                        TiltaksdeltakelsePeriodeDTO(
+                            eksternDeltagelseId = tiltaksdeltagelse.eksternDeltagelseId,
+                            periode = tiltaksdeltagelse.periode!!.toDTO(),
+                        ),
+                    ),
+                    avslagsgrunner = listOf(ValgtHjemmelForAvslagDTO.DeltarIkkePåArbeidsmarkedstiltak),
+                ),
+            )
+
+            val oppdatertBehandling = tac.behandlingContext.behandlingRepo.hent(behandling.id)
+
+            oppdatertBehandling.resultat.shouldBeInstanceOf<Avslag>()
+            oppdatertBehandling.fritekstTilVedtaksbrev!!.verdi shouldBe "ny brevtekst"
+            oppdatertBehandling.begrunnelseVilkårsvurdering!!.verdi shouldBe "ny begrunnelse"
+            (oppdatertBehandling.resultat as Avslag).avslagsgrunner shouldBe nonEmptySetOf(Avslagsgrunnlag.DeltarIkkePåArbeidsmarkedstiltak)
+        }
+    }
+
+    @Test
+    fun `kan oppdatere revurdering innvilgelse`() {
+        val tac = TestApplicationContext()
+
+        testApplication {
+            application {
+                jacksonSerialization()
+                routing { routes(tac) }
+            }
+
+            val (sak, _, _, revurdering) = startRevurderingInnvilgelse(tac)
+
+            val tiltaksdeltagelse = revurdering.saksopplysninger.tiltaksdeltagelse.first()
+            val nyInnvilgelsesperiode = tiltaksdeltagelse.periode!!.minusTilOgMed(1)
+
+            val barnetillegg = barnetillegg(
+                begrunnelse = BegrunnelseVilkårsvurdering("barnetillegg begrunnelse"),
+                periode = nyInnvilgelsesperiode,
+                antallBarn = AntallBarn(1),
+            )
+
+            val antallDager = SammenhengendePeriodisering(
+                AntallDagerForMeldeperiode(MAKS_DAGER_MED_TILTAKSPENGER_FOR_PERIODE),
+                nyInnvilgelsesperiode,
+            )
+
+            oppdaterBehandling(
+                tac = tac,
+                sakId = sak.id,
+                behandlingId = revurdering.id,
+                oppdaterBehandlingDTO = OppdaterRevurderingDTO.Innvilgelse(
+                    fritekstTilVedtaksbrev = "ny brevtekst",
+                    begrunnelseVilkårsvurdering = "ny begrunnelse",
+                    valgteTiltaksdeltakelser = listOf(
+                        TiltaksdeltakelsePeriodeDTO(
+                            eksternDeltagelseId = tiltaksdeltagelse.eksternDeltagelseId,
+                            periode = tiltaksdeltagelse.periode!!.toDTO(),
+                        ),
+                    ),
+                    innvilgelsesperiode = nyInnvilgelsesperiode.toDTO(),
+                    barnetillegg = barnetillegg.toBarnetilleggDTO(),
+                    antallDagerPerMeldeperiodeForPerioder = antallDager.toDTO(),
+                ),
+            )
+
+            val oppdatertBehandling = tac.behandlingContext.behandlingRepo.hent(revurdering.id)
+
+            oppdatertBehandling.resultat.shouldBeInstanceOf<RevurderingResultat.Innvilgelse>()
+            oppdatertBehandling.fritekstTilVedtaksbrev!!.verdi shouldBe "ny brevtekst"
+            oppdatertBehandling.begrunnelseVilkårsvurdering!!.verdi shouldBe "ny begrunnelse"
+            oppdatertBehandling.virkningsperiode shouldBe nyInnvilgelsesperiode
+            oppdatertBehandling.barnetillegg shouldBe barnetillegg
+            oppdatertBehandling.antallDagerPerMeldeperiode shouldBe antallDager
+        }
+    }
+
+    @Test
+    fun `kan oppdatere revurdering stans`() {
+        val tac = TestApplicationContext()
+
+        testApplication {
+            application {
+                jacksonSerialization()
+                routing { routes(tac) }
+            }
+
+            val (sak, _, _, revurdering) = startRevurderingStans(tac)
+
+            oppdaterBehandling(
+                tac = tac,
+                sakId = sak.id,
+                behandlingId = revurdering.id,
+                oppdaterBehandlingDTO = OppdaterRevurderingDTO.Stans(
+                    fritekstTilVedtaksbrev = "ny brevtekst",
+                    begrunnelseVilkårsvurdering = "ny begrunnelse",
+                    valgteHjemler = listOf(ValgtHjemmelForStansDTO.DeltarIkkePåArbeidsmarkedstiltak),
+                    stansFraOgMed = 9.april(2025),
+                ),
+            )
+
+            val oppdatertBehandling = tac.behandlingContext.behandlingRepo.hent(revurdering.id)
+
+            oppdatertBehandling.resultat.shouldBeInstanceOf<Stans>()
+            oppdatertBehandling.fritekstTilVedtaksbrev!!.verdi shouldBe "ny brevtekst"
+            oppdatertBehandling.begrunnelseVilkårsvurdering!!.verdi shouldBe "ny begrunnelse"
+            oppdatertBehandling.virkningsperiode!!.fraOgMed shouldBe 9.april(2025)
+            (oppdatertBehandling.resultat as Stans).valgtHjemmel shouldBe listOf(ValgtHjemmelForStans.DeltarIkkePåArbeidsmarkedstiltak)
         }
     }
 }
