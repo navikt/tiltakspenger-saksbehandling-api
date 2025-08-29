865f21a1e44432aa618dfc50b880969612312612
Tia Firing <tia.firing@nav.no>
2025-08-29 13:10:16 +0200

    fjerner ubrukt kode

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/meldekort/ports/BrukersMeldekortRepo.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/meldekort/ports/BrukersMeldekortRepo.kt
@@ -6,17 +6,10 @@ import no.nav.tiltakspenger.libs.meldekort.MeldeperiodeId
 import no.nav.tiltakspenger.libs.persistering.domene.SessionContext
 import no.nav.tiltakspenger.saksbehandling.meldekort.domene.BrukersMeldekort
 import no.nav.tiltakspenger.saksbehandling.meldekort.domene.BrukersMeldekortBehandletAutomatiskStatus
-import no.nav.tiltakspenger.saksbehandling.oppgave.OppgaveId
 
 interface BrukersMeldekortRepo {
     fun lagre(
         brukersMeldekort: BrukersMeldekort,
-        sessionContext: SessionContext? = null,
-    )
-
-    fun oppdaterOppgaveId(
-        meldekortId: MeldekortId,
-        oppgaveId: OppgaveId,
         sessionContext: SessionContext? = null,
     )
 
@@ -32,10 +25,6 @@ interface BrukersMeldekortRepo {
 
     fun hentForMeldeperiodeId(
         meldeperiodeId: MeldeperiodeId,
-        sessionContext: SessionContext? = null,
-    ): List<BrukersMeldekort>
-
-    fun hentMeldekortSomDetSkalOpprettesOppgaveFor(
         sessionContext: SessionContext? = null,
     ): List<BrukersMeldekort>
 
