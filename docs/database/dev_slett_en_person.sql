63e6b9d4b745a8608fde9274ca14d88693a87ca1
John Andre Hestad <john.andre.hestad@nav.no>
2025-09-05 10:11:22 +0200

    Oppdater manglende tabeller i dev_slett_en_person.sql

--- docs/database/dev_slett_en_person.sql
+++ docs/database/dev_slett_en_person.sql
@@ -1,5 +1,5 @@
 WITH
-sakz AS (DELETE FROM sak WHERE id = 'sak_01JQVBMZ7WX2VKEEK8XKV00WJ4' RETURNING id),
+sakz AS (DELETE FROM sak RETURNING id),
 behandlingz AS (DELETE FROM behandling WHERE sak_id IN (SELECT id FROM sakz) returning soknad_id),
 søknadz AS (DELETE FROM søknad WHERE sak_id IN (SELECT id FROM sakz) OR id in (select soknad_id from behandlingz) returning id),
 søknad_barnetilleggz AS (DELETE FROM søknad_barnetillegg WHERE søknad_id IN (SELECT id FROM søknadz)),
@@ -8,10 +8,15 @@ rammevedtakz AS (DELETE FROM rammevedtak WHERE sak_id IN (SELECT id FROM sakz)),
 meldekortz AS (DELETE FROM meldekortbehandling WHERE sak_id IN (SELECT id FROM sakz)),
 meldekort_brukerz AS (DELETE from meldekort_bruker mb WHERE sak_id IN (SELECT id FROM sakz)),
 meldekortvedtakz AS (DELETE FROM meldekortvedtak WHERE sak_id IN (SELECT id FROM sakz)),
+utbtalingsvedtakz AS (DELETE FROM utbetalingsvedtak WHERE sak_id IN (SELECT id FROM sakz)),
 meldekortbehandlingz AS (DELETE from meldekortbehandling mb where sak_id IN (SELECT id FROM sakz)),
 meldeperiode AS (delete from meldeperiode m WHERE sak_id IN (SELECT id FROM sakz)),
+utbetalingz as (delete from utbetaling u where sak_id in (select id from sakz)),
 statistikk_utbetalingz AS (DELETE FROM statistikk_utbetaling WHERE sak_id IN (SELECT id FROM sakz)),
+statistikk_meldekortz AS (DELETE FROM statistikk_meldekort WHERE sak_id IN (SELECT id FROM sakz)),
 statistikk_stonadz AS (DELETE FROM statistikk_stonad WHERE sak_id IN (SELECT id FROM sakz)),
 statistikk_sakz AS (DELETE FROM statistikk_sak WHERE sak_id IN (SELECT id FROM sakz) returning id),
+personhendelsez AS (DELETE FROM personhendelse WHERE sak_id IN (SELECT id FROM sakz) returning id),
+identhendelsez AS (DELETE FROM identhendelse WHERE sak_id IN (SELECT id FROM sakz) returning id),
 tiltaksdeltaker_kafkaz AS (DELETE FROM tiltaksdeltaker_kafka WHERE sak_id IN (SELECT id FROM sakz))
 SELECT id FROM sakz;
