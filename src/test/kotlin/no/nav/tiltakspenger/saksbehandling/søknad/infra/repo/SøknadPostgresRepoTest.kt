0eea301e3b2c2df855f5801b2046b8947d104b93
Henrik Solum <henrik.solum@nav.no>
2025-09-29 11:11:29 +0200

    1343 Tester for lagring av søknader

---
+++ src/test/kotlin/no/nav/tiltakspenger/saksbehandling/søknad/infra/repo/SøknadPostgresRepoTest.kt
@@ -0,0 +1,308 @@
+package no.nav.tiltakspenger.saksbehandling.søknad.infra.repo
+
+import io.kotest.matchers.shouldBe
+import no.nav.tiltakspenger.libs.common.Fnr
+import no.nav.tiltakspenger.libs.common.random
+import no.nav.tiltakspenger.libs.common.singleOrNullOrThrow
+import no.nav.tiltakspenger.libs.dato.januar
+import no.nav.tiltakspenger.libs.dato.mars
+import no.nav.tiltakspenger.libs.periodisering.Periode
+import no.nav.tiltakspenger.saksbehandling.infra.repo.persisterSakOgSøknad
+import no.nav.tiltakspenger.saksbehandling.infra.repo.withMigratedDb
+import no.nav.tiltakspenger.saksbehandling.objectmothers.ObjectMother
+import no.nav.tiltakspenger.saksbehandling.objectmothers.ObjectMother.fraOgMedDatoJa
+import no.nav.tiltakspenger.saksbehandling.objectmothers.ObjectMother.fraOgMedDatoNei
+import no.nav.tiltakspenger.saksbehandling.objectmothers.ObjectMother.ja
+import no.nav.tiltakspenger.saksbehandling.objectmothers.ObjectMother.nei
+import no.nav.tiltakspenger.saksbehandling.objectmothers.ObjectMother.periodeJa
+import no.nav.tiltakspenger.saksbehandling.objectmothers.ObjectMother.periodeNei
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class SøknadPostgresRepoTest {
+    @Nested
+    inner class LagreSøknad {
+        @Test
+        fun `lagre og hente en søknad`() {
+            withMigratedDb { testDataHelper ->
+                val søknadRepo = testDataHelper.søknadRepo
+                val fnr = Fnr.random()
+                val søknad = testDataHelper.persisterSakOgSøknad(fnr = fnr)
+
+                søknadRepo.hentSøknaderForFnr(fnr).also {
+                    it.singleOrNullOrThrow() shouldBe søknad
+                }
+            }
+        }
+
+        @Test
+        fun `søknad med nei på alle spørsmål`() {
+            withMigratedDb { testDataHelper ->
+                val søknadRepo = testDataHelper.søknadRepo
+                val fnr = Fnr.random()
+                val deltakelseFom = 1.januar(2023)
+                val deltakelseTom = 31.mars(2023)
+                val tiltak = ObjectMother.søknadstiltak(deltakelseFom = deltakelseFom, deltakelseTom = deltakelseTom)
+
+                ObjectMother.nySøknad(
+                    periode = Periode(deltakelseFom, deltakelseTom),
+                    søknadstiltak = tiltak,
+                    fnr = fnr,
+                    kvp = periodeNei(),
+                    intro = periodeNei(),
+                    institusjon = periodeNei(),
+                    trygdOgPensjon = periodeNei(),
+                    etterlønn = nei(),
+                    gjenlevendepensjon = periodeNei(),
+                    alderspensjon = fraOgMedDatoNei(),
+                    sykepenger = periodeNei(),
+                    supplerendeStønadAlder = periodeNei(),
+                    supplerendeStønadFlyktning = periodeNei(),
+                    jobbsjansen = periodeNei(),
+                )
+
+                val persistertSøknad = testDataHelper.persisterSakOgSøknad(fnr = fnr)
+
+                søknadRepo.hentSøknaderForFnr(fnr).also { søknader ->
+                    søknader.single().also { hentetSøknad ->
+                        hentetSøknad shouldBe persistertSøknad
+                        hentetSøknad.tiltak shouldBe persistertSøknad.tiltak
+                        // Burde være dekket av objektsammenligningen, men i tilfelle den skulle brekke!
+                        hentetSøknad.kvp shouldBe periodeNei()
+                        hentetSøknad.intro shouldBe periodeNei()
+                        hentetSøknad.institusjon shouldBe periodeNei()
+                        hentetSøknad.trygdOgPensjon shouldBe periodeNei()
+                        hentetSøknad.etterlønn shouldBe nei()
+                        hentetSøknad.gjenlevendepensjon shouldBe periodeNei()
+                        hentetSøknad.alderspensjon shouldBe fraOgMedDatoNei()
+                        hentetSøknad.sykepenger shouldBe periodeNei()
+                        hentetSøknad.supplerendeStønadAlder shouldBe periodeNei()
+                        hentetSøknad.supplerendeStønadFlyktning shouldBe periodeNei()
+                        hentetSøknad.jobbsjansen shouldBe periodeNei()
+                    }
+                }
+            }
+        }
+
+        @Nested
+        inner class JaNeiSpm {
+            @Test
+            fun `kan svare ja`() {
+                withMigratedDb { testDataHelper ->
+                    val søknadRepo = testDataHelper.søknadRepo
+                    val fnr = Fnr.random()
+                    val sak = ObjectMother.nySak(fnr = fnr, saksnummer = testDataHelper.saksnummerGenerator.neste())
+                    val tiltak =
+                        ObjectMother.søknadstiltak(deltakelseFom = 1.januar(2023), deltakelseTom = 31.mars(2023))
+                    val søknad = ObjectMother.nySøknad(
+                        periode = Periode(tiltak.deltakelseFom, tiltak.deltakelseTom),
+                        sakId = sak.id,
+                        saksnummer = sak.saksnummer,
+                        søknadstiltak = tiltak,
+                        fnr = fnr,
+                        etterlønn = ja(),
+                    )
+
+                    val persistertSøknad = testDataHelper.persisterSakOgSøknad(fnr = fnr, sak = sak, søknad = søknad)
+
+                    søknadRepo.hentSøknaderForFnr(fnr).also { søknader ->
+                        søknader.single().also { hentetSøknad ->
+                            hentetSøknad shouldBe persistertSøknad
+                            hentetSøknad.tiltak shouldBe persistertSøknad.tiltak
+                            // Burde være dekket av objektsammenligningen, men i tilfelle den skulle brekke!
+                            hentetSøknad.etterlønn shouldBe ja()
+                        }
+                    }
+                }
+            }
+
+            @Test
+            fun `kan svare nei`() {
+                withMigratedDb { testDataHelper ->
+                    val søknadRepo = testDataHelper.søknadRepo
+                    val fnr = Fnr.random()
+                    val sak = ObjectMother.nySak(fnr = fnr, saksnummer = testDataHelper.saksnummerGenerator.neste())
+                    val tiltak =
+                        ObjectMother.søknadstiltak(deltakelseFom = 1.januar(2023), deltakelseTom = 31.mars(2023))
+                    val søknad = ObjectMother.nySøknad(
+                        periode = Periode(tiltak.deltakelseFom, tiltak.deltakelseTom),
+                        sakId = sak.id,
+                        saksnummer = sak.saksnummer,
+                        søknadstiltak = tiltak,
+                        fnr = fnr,
+                        etterlønn = nei(),
+                    )
+
+                    val persistertSøknad = testDataHelper.persisterSakOgSøknad(fnr = fnr, sak = sak, søknad = søknad)
+
+                    søknadRepo.hentSøknaderForFnr(fnr).also { søknader ->
+                        søknader.single().also { hentetSøknad ->
+                            hentetSøknad shouldBe persistertSøknad
+                            hentetSøknad.tiltak shouldBe persistertSøknad.tiltak
+                            // Burde være dekket av objektsammenligningen, men i tilfelle den skulle brekke!
+                            hentetSøknad.etterlønn shouldBe nei()
+                        }
+                    }
+                }
+            }
+        }
+
+        @Nested
+        inner class PeriodeSpm {
+            @Test
+            fun `kan svare ja`() {
+                withMigratedDb { testDataHelper ->
+                    val søknadRepo = testDataHelper.søknadRepo
+                    val fnr = Fnr.random()
+                    val sak = ObjectMother.nySak(fnr = fnr, saksnummer = testDataHelper.saksnummerGenerator.neste())
+                    val tiltak =
+                        ObjectMother.søknadstiltak(deltakelseFom = 1.januar(2023), deltakelseTom = 31.mars(2023))
+                    val søknad = ObjectMother.nySøknad(
+                        periode = Periode(tiltak.deltakelseFom, tiltak.deltakelseTom),
+                        sakId = sak.id,
+                        saksnummer = sak.saksnummer,
+                        søknadstiltak = tiltak,
+                        fnr = fnr,
+                        kvp = periodeJa(),
+                        intro = periodeJa(),
+                        institusjon = periodeJa(),
+                        trygdOgPensjon = periodeJa(),
+                        gjenlevendepensjon = periodeJa(),
+                        sykepenger = periodeJa(),
+                        supplerendeStønadAlder = periodeJa(),
+                        supplerendeStønadFlyktning = periodeJa(),
+                        jobbsjansen = periodeJa(),
+                    )
+
+                    val persistertSøknad = testDataHelper.persisterSakOgSøknad(fnr = fnr, sak = sak, søknad = søknad)
+
+                    søknadRepo.hentSøknaderForFnr(fnr).also { søknader ->
+                        søknader.single().also { hentetSøknad ->
+                            hentetSøknad shouldBe persistertSøknad
+                            hentetSøknad.tiltak shouldBe persistertSøknad.tiltak
+                            // Burde være dekket av objektsammenligningen, men i tilfelle den skulle brekke!
+                            hentetSøknad.kvp shouldBe periodeJa()
+                            hentetSøknad.intro shouldBe periodeJa()
+                            hentetSøknad.institusjon shouldBe periodeJa()
+                            hentetSøknad.trygdOgPensjon shouldBe periodeJa()
+                            hentetSøknad.gjenlevendepensjon shouldBe periodeJa()
+                            hentetSøknad.sykepenger shouldBe periodeJa()
+                            hentetSøknad.supplerendeStønadAlder shouldBe periodeJa()
+                            hentetSøknad.supplerendeStønadFlyktning shouldBe periodeJa()
+                            hentetSøknad.jobbsjansen shouldBe periodeJa()
+                        }
+                    }
+                }
+            }
+
+            @Test
+            fun `kan svare nei`() {
+                withMigratedDb { testDataHelper ->
+                    val søknadRepo = testDataHelper.søknadRepo
+                    val fnr = Fnr.random()
+                    val sak = ObjectMother.nySak(fnr = fnr, saksnummer = testDataHelper.saksnummerGenerator.neste())
+                    val tiltak =
+                        ObjectMother.søknadstiltak(deltakelseFom = 1.januar(2023), deltakelseTom = 31.mars(2023))
+                    val søknad = ObjectMother.nySøknad(
+                        periode = Periode(tiltak.deltakelseFom, tiltak.deltakelseTom),
+                        sakId = sak.id,
+                        saksnummer = sak.saksnummer,
+                        søknadstiltak = tiltak,
+                        fnr = fnr,
+                        kvp = periodeNei(),
+                        intro = periodeNei(),
+                        institusjon = periodeNei(),
+                        trygdOgPensjon = periodeNei(),
+                        gjenlevendepensjon = periodeNei(),
+                        sykepenger = periodeNei(),
+                        supplerendeStønadAlder = periodeNei(),
+                        supplerendeStønadFlyktning = periodeNei(),
+                        jobbsjansen = periodeNei(),
+                    )
+
+                    val persistertSøknad = testDataHelper.persisterSakOgSøknad(fnr = fnr, sak = sak, søknad = søknad)
+
+                    søknadRepo.hentSøknaderForFnr(fnr).also { søknader ->
+                        søknader.single().also { hentetSøknad ->
+                            hentetSøknad shouldBe persistertSøknad
+                            hentetSøknad.tiltak shouldBe persistertSøknad.tiltak
+                            // Burde være dekket av objektsammenligningen, men i tilfelle den skulle brekke!
+                            hentetSøknad.kvp shouldBe periodeNei()
+                            hentetSøknad.intro shouldBe periodeNei()
+                            hentetSøknad.institusjon shouldBe periodeNei()
+                            hentetSøknad.trygdOgPensjon shouldBe periodeNei()
+                            hentetSøknad.gjenlevendepensjon shouldBe periodeNei()
+                            hentetSøknad.sykepenger shouldBe periodeNei()
+                            hentetSøknad.supplerendeStønadAlder shouldBe periodeNei()
+                            hentetSøknad.supplerendeStønadFlyktning shouldBe periodeNei()
+                            hentetSøknad.jobbsjansen shouldBe periodeNei()
+                        }
+                    }
+                }
+            }
+        }
+
+        @Nested
+        inner class FraOgMedDatoSpm {
+            @Test
+            fun `kan svare ja`() {
+                withMigratedDb { testDataHelper ->
+                    val søknadRepo = testDataHelper.søknadRepo
+                    val fnr = Fnr.random()
+                    val sak = ObjectMother.nySak(fnr = fnr, saksnummer = testDataHelper.saksnummerGenerator.neste())
+                    val tiltak =
+                        ObjectMother.søknadstiltak(deltakelseFom = 1.januar(2023), deltakelseTom = 31.mars(2023))
+                    val søknad = ObjectMother.nySøknad(
+                        periode = Periode(tiltak.deltakelseFom, tiltak.deltakelseTom),
+                        sakId = sak.id,
+                        saksnummer = sak.saksnummer,
+                        søknadstiltak = tiltak,
+                        fnr = fnr,
+                        alderspensjon = fraOgMedDatoJa(),
+                    )
+
+                    val persistertSøknad = testDataHelper.persisterSakOgSøknad(fnr = fnr, sak = sak, søknad = søknad)
+
+                    søknadRepo.hentSøknaderForFnr(fnr).also { søknader ->
+                        søknader.single().also { hentetSøknad ->
+                            hentetSøknad shouldBe persistertSøknad
+                            hentetSøknad.tiltak shouldBe persistertSøknad.tiltak
+                            // Burde være dekket av objektsammenligningen, men i tilfelle den skulle brekke!
+                            hentetSøknad.alderspensjon shouldBe fraOgMedDatoJa()
+                        }
+                    }
+                }
+            }
+
+            @Test
+            fun `kan svare nei`() {
+                withMigratedDb { testDataHelper ->
+                    val søknadRepo = testDataHelper.søknadRepo
+                    val fnr = Fnr.random()
+                    val sak = ObjectMother.nySak(fnr = fnr, saksnummer = testDataHelper.saksnummerGenerator.neste())
+                    val tiltak =
+                        ObjectMother.søknadstiltak(deltakelseFom = 1.januar(2023), deltakelseTom = 31.mars(2023))
+                    val søknad = ObjectMother.nySøknad(
+                        periode = Periode(tiltak.deltakelseFom, tiltak.deltakelseTom),
+                        sakId = sak.id,
+                        saksnummer = sak.saksnummer,
+                        søknadstiltak = tiltak,
+                        fnr = fnr,
+                        alderspensjon = fraOgMedDatoNei(),
+                    )
+
+                    val persistertSøknad = testDataHelper.persisterSakOgSøknad(fnr = fnr, sak = sak, søknad = søknad)
+
+                    søknadRepo.hentSøknaderForFnr(fnr).also { søknader ->
+                        søknader.single().also { hentetSøknad ->
+                            hentetSøknad shouldBe persistertSøknad
+                            hentetSøknad.tiltak shouldBe persistertSøknad.tiltak
+                            // Burde være dekket av objektsammenligningen, men i tilfelle den skulle brekke!
+                            hentetSøknad.alderspensjon shouldBe fraOgMedDatoNei()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
