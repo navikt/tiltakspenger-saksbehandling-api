f771e07cd7a45e6ea2ff3060d452c4635f6a73ef
Henrik Solum <henrik.solum@nav.no>
2025-10-06 14:31:17 +0200

    1343 Slår sammen spørringer for å hente person fra pdl
    Feltene i metadata som var forskjellige ble ikke brukt og den eneste andre forskjellen var at den ene ikke hentet fødselsdato. Det har vi behandlingsgrunnlag for uansett så det er ikke verdt å vedlikeholde to såpass like spørringer.

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/person/EnkelPerson.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/person/EnkelPerson.kt
@@ -1,9 +1,11 @@
 package no.nav.tiltakspenger.saksbehandling.person
 
 import no.nav.tiltakspenger.libs.common.Fnr
+import java.time.LocalDate
 
 data class EnkelPerson(
     val fnr: Fnr,
+    val fødselsdato: LocalDate,
     val fornavn: String,
     val mellomnavn: String?,
     val etternavn: String,
