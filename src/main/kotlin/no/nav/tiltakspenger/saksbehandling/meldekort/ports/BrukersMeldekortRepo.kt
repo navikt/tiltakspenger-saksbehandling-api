eaa792fd9262a9a5fa61ebda8e9a2842524a6a0b
Anders Nomerstad <anders@nomerstad.com>
2025-10-08 10:27:53 +0200

    Skal ikke flagge korrigeringer fra bruker for automatisk behandling

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/meldekort/ports/BrukersMeldekortRepo.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/meldekort/ports/BrukersMeldekortRepo.kt
@@ -3,6 +3,7 @@ package no.nav.tiltakspenger.saksbehandling.meldekort.ports
 import no.nav.tiltakspenger.libs.common.MeldekortId
 import no.nav.tiltakspenger.libs.common.SakId
 import no.nav.tiltakspenger.libs.meldekort.MeldeperiodeId
+import no.nav.tiltakspenger.libs.meldekort.MeldeperiodeKjedeId
 import no.nav.tiltakspenger.libs.persistering.domene.SessionContext
 import no.nav.tiltakspenger.saksbehandling.meldekort.domene.BrukersMeldekort
 import no.nav.tiltakspenger.saksbehandling.meldekort.domene.BrukersMeldekortBehandletAutomatiskStatus
@@ -25,6 +26,12 @@ interface BrukersMeldekortRepo {
 
     fun hentForMeldeperiodeId(
         meldeperiodeId: MeldeperiodeId,
+        sessionContext: SessionContext? = null,
+    ): List<BrukersMeldekort>
+
+    fun hentForKjedeId(
+        kjedeId: MeldeperiodeKjedeId,
+        sakId: SakId,
         sessionContext: SessionContext? = null,
     ): List<BrukersMeldekort>
 
