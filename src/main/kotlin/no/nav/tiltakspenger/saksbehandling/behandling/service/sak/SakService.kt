4c81e2a148fa2c1526c122ade58fab167c00d8ff
Henrik Solum <henrik.solum@nav.no>
2025-10-09 09:53:42 +0200

    1343 Faila merge conflict
    lå visst igjen noen ubrukte (nå) ugyldige imports

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/service/sak/SakService.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/service/sak/SakService.kt
@@ -10,8 +10,6 @@ import no.nav.tiltakspenger.libs.common.Fnr
 import no.nav.tiltakspenger.libs.common.SakId
 import no.nav.tiltakspenger.libs.persistering.domene.SessionContext
 import no.nav.tiltakspenger.libs.personklient.skjerming.FellesSkjermingsklient
-import no.nav.tiltakspenger.saksbehandling.behandling.domene.Behandlinger
-import no.nav.tiltakspenger.saksbehandling.behandling.domene.KanIkkeOppretteBehandling
 import no.nav.tiltakspenger.saksbehandling.behandling.domene.Rammebehandlinger
 import no.nav.tiltakspenger.saksbehandling.behandling.ports.SakRepo
 import no.nav.tiltakspenger.saksbehandling.behandling.service.person.KunneIkkeHenteEnkelPerson
