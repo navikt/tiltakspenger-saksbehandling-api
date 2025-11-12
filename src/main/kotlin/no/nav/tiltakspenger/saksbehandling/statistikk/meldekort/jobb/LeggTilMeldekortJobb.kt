365748d0a3579e4e7c4a23e87ecbe98e7b0930ba
tiafiring <tia.firing@nav.no>
2025-11-12 12:33:59 +0100

    1726 lagrer meldekort som har blitt overskrevet i statistikktabellen … (#1294)
    * 1726 lagrer meldekort som har blitt overskrevet i statistikktabellen på nytt
    
    * 1726 fjerner session pga feil

---
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/statistikk/meldekort/jobb/LeggTilMeldekortJobb.kt
@@ -0,0 +1,60 @@
+package no.nav.tiltakspenger.saksbehandling.statistikk.meldekort.jobb
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotliquery.queryOf
+import no.nav.tiltakspenger.libs.common.MeldekortId
+import no.nav.tiltakspenger.libs.persistering.infrastruktur.PostgresSessionFactory
+import no.nav.tiltakspenger.saksbehandling.meldekort.ports.MeldekortBehandlingRepo
+import no.nav.tiltakspenger.saksbehandling.statistikk.meldekort.StatistikkMeldekortRepo
+import no.nav.tiltakspenger.saksbehandling.statistikk.meldekort.tilStatistikkMeldekortDTO
+import java.time.Clock
+
+class LeggTilMeldekortJobb(
+    private val sessionFactory: PostgresSessionFactory,
+    private val clock: Clock,
+    private val meldekortBehandlingRepo: MeldekortBehandlingRepo,
+    private val statistikkMeldekortRepo: StatistikkMeldekortRepo,
+) {
+    private val log = KotlinLogging.logger { }
+
+    // Jobb for å legge til godkjente meldekortbehandlinger i statistikk-tabellen som har blitt overskrevet.
+    // Skal slettes når den har lagret det som mangler.
+    fun leggTilManglendeMeldekortStatistikk() {
+        // hent alle godkjente korrigerte meldekortbehandlinger
+        val godkjenteKorrigerteMeldekortbehandlinger = meldekortBehandlingRepo.hentGodkjenteKorrigerteBehandlinger()
+        log.info { "Fant ${godkjenteKorrigerteMeldekortbehandlinger.size} godkjente korrigerte meldekort" }
+
+        // henter meldekortbehandlinger for sak for å finne alle meldekortbehandlinger for samme kjede
+        godkjenteKorrigerteMeldekortbehandlinger.forEach { korrigertBehandling ->
+            log.info { "Henter meldekortbehandlinger for sak ${korrigertBehandling.sakId} for korrigert meldekort ${korrigertBehandling.id}" }
+            val meldekortbehandlinger = meldekortBehandlingRepo.hentForSakId(korrigertBehandling.sakId)
+                ?: throw IllegalStateException("Fant ikke meldekortbehandlingen vi nettopp hentet, sakId ${korrigertBehandling.sakId}")
+
+            val andreGodkjenteMeldekortbehandlingerSammeKjede = meldekortbehandlinger.godkjenteMeldekort.filter {
+                it.id != korrigertBehandling.id && it.kjedeId == korrigertBehandling.kjedeId
+            }
+
+            andreGodkjenteMeldekortbehandlingerSammeKjede.forEach {
+                // sjekk om finnes i statistikk_meldekort, hvis ikke: lagre
+                if (!finnesIMeldekortStatisikk(it.id)) {
+                    statistikkMeldekortRepo.lagre(it.tilStatistikkMeldekortDTO(clock))
+                    log.info { "Lagret statistikkinnslag for meldekortbehandling med id ${it.id}" }
+                } else {
+                    log.info { "Meldekortbehandling med id ${it.id} finnes i statistikktabellen fra før" }
+                }
+            }
+            log.info { "Ferdig med å opprette statistikk for ${andreGodkjenteMeldekortbehandlingerSammeKjede.size} behandlinger for korrigert meldekort ${korrigertBehandling.id}" }
+        }
+    }
+
+    private fun finnesIMeldekortStatisikk(meldekortId: MeldekortId): Boolean {
+        return sessionFactory.withSession { session ->
+            session.run(
+                queryOf(
+                    "select exists(select 1 from statistikk_meldekort where meldekortbehandling_id = :meldekortbehandling_id)",
+                    "meldekortbehandling_id" to meldekortId.toString(),
+                ).map { row -> row.boolean("exists") }.asSingle,
+            ) ?: false
+        }
+    }
+}
