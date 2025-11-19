0df1cf389fd0b0d31336d714d4db3829d10db0ef
Henrik Solum <henrik.solum@nav.no>
2025-11-19 12:55:30 +0100

    1343 Begrenser oppslag på tiltaksdeltakelse til perioden man sender inn
    For papirsøknad blir dette perioden bruker har søkt på

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/tiltaksdeltakelse/service/KunneIkkeHenteTiltaksdeltakelser.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/tiltaksdeltakelse/service/KunneIkkeHenteTiltaksdeltakelser.kt
@@ -2,4 +2,6 @@ package no.nav.tiltakspenger.saksbehandling.tiltaksdeltakelse.service
 
 sealed interface KunneIkkeHenteTiltaksdeltakelser {
     data object FeilVedKallMotPdl : KunneIkkeHenteTiltaksdeltakelser
+    data object OppslagsperiodeMangler : KunneIkkeHenteTiltaksdeltakelser
+    data object NegativOppslagsperiode : KunneIkkeHenteTiltaksdeltakelser
 }
--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/tiltaksdeltakelse/service/TiltaksdeltakelseService.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/tiltaksdeltakelse/service/TiltaksdeltakelseService.kt
@@ -6,10 +6,12 @@ import arrow.core.left
 import arrow.core.right
 import no.nav.tiltakspenger.libs.common.CorrelationId
 import no.nav.tiltakspenger.libs.common.SakId
+import no.nav.tiltakspenger.libs.periodisering.Periode
 import no.nav.tiltakspenger.saksbehandling.behandling.service.person.PersonService
 import no.nav.tiltakspenger.saksbehandling.behandling.service.sak.SakService
 import no.nav.tiltakspenger.saksbehandling.tiltaksdeltakelse.TiltaksdeltakelseMedArrangørnavn
 import no.nav.tiltakspenger.saksbehandling.tiltaksdeltakelse.infra.TiltaksdeltakelseKlient
+import java.time.LocalDate
 
 class TiltaksdeltakelseService(
     private val sakService: SakService,
@@ -18,18 +20,34 @@ class TiltaksdeltakelseService(
 ) {
     suspend fun hentTiltaksdeltakelserForSak(
         sakId: SakId,
+        fraOgMed: LocalDate?,
+        tilOgMed: LocalDate?,
         correlationId: CorrelationId,
-    ): Either<KunneIkkeHenteTiltaksdeltakelser.FeilVedKallMotPdl, List<TiltaksdeltakelseMedArrangørnavn>> {
+    ): Either<KunneIkkeHenteTiltaksdeltakelser, List<TiltaksdeltakelseMedArrangørnavn>> {
+        if (fraOgMed == null || tilOgMed == null) {
+            return KunneIkkeHenteTiltaksdeltakelser.OppslagsperiodeMangler.left()
+        }
+
+        if (!fraOgMed.isAfter(tilOgMed)) {
+            return KunneIkkeHenteTiltaksdeltakelser.NegativOppslagsperiode.left()
+        }
+
+        val oppslagsperiode = Periode(fraOgMed, tilOgMed)
         val sak = sakService.hentForSakId(sakId)
 
         val person = personService.hentEnkelPersonFnr(sak.fnr)
             .getOrElse { return KunneIkkeHenteTiltaksdeltakelser.FeilVedKallMotPdl.left() }
         val harAdressebeskyttelse = person.fortrolig || person.strengtFortrolig || person.strengtFortroligUtland
 
-        return tiltaksdeltakelseKlient.hentTiltaksdeltakelserMedArrangørnavn(
+        val alleTiltaksdeltakelser = tiltaksdeltakelseKlient.hentTiltaksdeltakelserMedArrangørnavn(
             fnr = sak.fnr,
             harAdressebeskyttelse = harAdressebeskyttelse,
             correlationId = correlationId,
-        ).right()
+        )
+
+        // Viser bare tiltaksdeltakelser som overlapper med perioden, tiltak med manglende fom og tom vises ikke.
+        return alleTiltaksdeltakelser.filter {
+            it.periode?.overlapperMed(oppslagsperiode) == true
+        }.right()
     }
 }
