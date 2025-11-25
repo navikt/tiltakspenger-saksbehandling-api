0f28bed4ede927b8a05e6326056f0b929edeb697
Henrik Solum <henrik.solum@nav.no>
2025-11-25 10:37:46 +0100

    1773 Applikasjonslim

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/sak/infra/routes/SakRoutes.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/sak/infra/routes/SakRoutes.kt
@@ -4,6 +4,7 @@ import io.ktor.server.routing.Route
 import no.nav.tiltakspenger.saksbehandling.auditlog.AuditService
 import no.nav.tiltakspenger.saksbehandling.auth.tilgangskontroll.TilgangskontrollService
 import no.nav.tiltakspenger.saksbehandling.behandling.service.avslutt.AvbrytSøknadOgBehandlingService
+import no.nav.tiltakspenger.saksbehandling.behandling.service.person.PersonService
 import no.nav.tiltakspenger.saksbehandling.behandling.service.sak.SakService
 import java.time.Clock
 
@@ -15,11 +16,12 @@ fun Route.sakRoutes(
     avbrytSøknadOgBehandlingService: AvbrytSøknadOgBehandlingService,
     clock: Clock,
     tilgangskontrollService: TilgangskontrollService,
+    personService: PersonService,
 ) {
     hentSakRoute(sakService, auditService, clock, tilgangskontrollService)
     hentSakForSaksnummerRoute(sakService, auditService, clock, tilgangskontrollService)
     hentEllerOpprettSakSystembrukerRoute(sakService)
-    hentEllerOpprettSakRoute(sakService, auditService, tilgangskontrollService)
+    hentEllerOpprettSakRoute(sakService, auditService, tilgangskontrollService, personService)
     toggleKanSendeHelgForMeldekortSakRoute(sakService, auditService, clock, tilgangskontrollService)
     // TODO jah: Denne føles litt malplassert.
     avbrytSøknadOgBehandling(auditService, avbrytSøknadOgBehandlingService, clock, tilgangskontrollService)
