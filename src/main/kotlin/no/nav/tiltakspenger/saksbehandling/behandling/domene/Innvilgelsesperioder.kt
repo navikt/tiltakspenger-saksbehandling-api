dc570ac5a857a697369f5153baf5334f46862264
Anders Nomerstad <anders@nomerstad.com>
2026-01-15 10:19:34 +0100

    Oppdaterer libs

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/domene/Innvilgelsesperioder.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/domene/Innvilgelsesperioder.kt
@@ -3,7 +3,6 @@ package no.nav.tiltakspenger.saksbehandling.behandling.domene
 import no.nav.tiltakspenger.libs.periodisering.IkkeTomPeriodisering
 import no.nav.tiltakspenger.libs.periodisering.Periode
 import no.nav.tiltakspenger.libs.periodisering.PeriodeMedVerdi
-import no.nav.tiltakspenger.libs.periodisering.Periodisering
 import no.nav.tiltakspenger.libs.periodisering.tilIkkeTomPeriodisering
 import no.nav.tiltakspenger.libs.periodisering.trekkFra
 import no.nav.tiltakspenger.saksbehandling.behandling.domene.saksopplysninger.Saksopplysninger
