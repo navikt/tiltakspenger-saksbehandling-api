f911bbf11178bc09e34400cc2bc5485108e70545
John Andre Hestad <john.andre.hestad@nav.no>
2026-01-15 13:59:31 +0100

    Endre test `lagre og hente en klagebehandling` til å bruke withTestApplicationContextAndPostgres
    Tester nå og brevtekst

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/klage/domene/brev/TittelOgTekst.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/klage/domene/brev/TittelOgTekst.kt
@@ -5,4 +5,11 @@ import no.nav.tiltakspenger.libs.common.NonBlankString
 data class TittelOgTekst(
     val tittel: NonBlankString,
     val tekst: NonBlankString,
-)
+) {
+    companion object {
+        operator fun invoke(tittel: String, tekst: String) = TittelOgTekst(
+            tittel = NonBlankString.create(tittel),
+            tekst = NonBlankString.create(tekst),
+        )
+    }
+}
--- src/test/kotlin/no/nav/tiltakspenger/saksbehandling/common/TestApplicationContextEx.kt
+++ src/test/kotlin/no/nav/tiltakspenger/saksbehandling/common/TestApplicationContextEx.kt
@@ -12,7 +12,30 @@ import no.nav.tiltakspenger.saksbehandling.auth.infra.TexasClientFake
 import no.nav.tiltakspenger.saksbehandling.auth.tilgangskontroll.infra.TilgangsmaskinFakeTestClient
 import no.nav.tiltakspenger.saksbehandling.fixedClock
 import no.nav.tiltakspenger.saksbehandling.fixedClockAt
+import no.nav.tiltakspenger.saksbehandling.infra.repo.TestDatabaseManager
+import no.nav.tiltakspenger.saksbehandling.infra.repo.withMigratedDb
 import no.nav.tiltakspenger.saksbehandling.infra.setup.ktorSetup
+
+private val dbManager = TestDatabaseManager()
+fun withTestApplicationContextAndPostgres(
+    additionalConfig: Application.() -> Unit = {},
+    clock: TikkendeKlokke = TikkendeKlokke(fixedClockAt(1.mai(2025))),
+    texasClient: TexasClient = TexasClientFake(clock),
+    tilgangsmaskinFakeClient: TilgangsmaskinFakeTestClient = TilgangsmaskinFakeTestClient(),
+    runIsolated: Boolean = false,
+    testBlock: suspend ApplicationTestBuilder.(TestApplicationContext) -> Unit,
+) {
+    dbManager.withMigratedDb(runIsolated = runIsolated, clock = clock) { testDataHelper ->
+        withTestApplicationContext(
+            additionalConfig = additionalConfig,
+            clock = clock,
+            texasClient = texasClient,
+            sessionFactory = testDataHelper.sessionFactory,
+            tilgangsmaskinFakeClient = tilgangsmaskinFakeClient,
+            testBlock = testBlock,
+        )
+    }
+}
 
 /**
  * @param clock Merk at vi ikke kan behandle et meldekort før vi har passert meldekortets første dag. Derfor er det viktig at [clock] er satt til en dato etter den første meldekortdagen i testene som bruker denne.
