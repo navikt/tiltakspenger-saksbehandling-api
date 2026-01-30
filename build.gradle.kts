65e77e084cf5055091da5b743707d6d5f57144e5
Tia Firing <tia.firing@nav.no>
2026-01-30 10:53:47 +0100

    legger til manglende dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,6 +95,7 @@ dependencies {
     // Caffeine
     implementation("com.github.ben-manes.caffeine:caffeine:3.2.3")
 
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.3.0")
     testImplementation(platform("org.junit:junit-bom:6.0.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
