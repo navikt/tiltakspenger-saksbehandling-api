2c55bb2f2d7edcf072f000e069bc824fa184e46b
Anders Nomerstad <anders@nomerstad.com>
2026-02-03 15:30:53 +0100

    Fjerner sql periode type :,|

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/infra/repo/dto/PeriodeDbJson.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/infra/repo/dto/PeriodeDbJson.kt
@@ -1,8 +1,6 @@
 package no.nav.tiltakspenger.saksbehandling.infra.repo.dto
 
-import kotliquery.Row
 import no.nav.tiltakspenger.libs.periode.Periode
-import org.postgresql.util.PGobject
 import java.time.LocalDate
 
 /**
@@ -16,30 +14,3 @@ data class PeriodeDbJson(
 }
 
 fun Periode.toDbJson(): PeriodeDbJson = PeriodeDbJson(fraOgMed.toString(), tilOgMed.toString())
-
-/**
- *  Serialize til sql type periode_datoer
- * */
-fun Periode.tilDbPeriode(): String {
-    return "(${this.fraOgMed},${this.tilOgMed})"
-}
-
-fun Row.periode(column: String): Periode {
-    return periodeOrNull(column)!!
-}
-
-fun Row.periodeOrNull(column: String): Periode? {
-    val pgObject = underlying.getObject(column, PGobject::class.java)
-    val value = pgObject?.value ?: return null
-    return parsePeriode(value)
-}
-
-private fun parsePeriode(value: String): Periode {
-    val (fraOgMed, tilOgMed) = value
-        .removeSurrounding("(", ")")
-        .split(",")
-    return Periode(
-        LocalDate.parse(fraOgMed),
-        LocalDate.parse(tilOgMed),
-    )
-}
