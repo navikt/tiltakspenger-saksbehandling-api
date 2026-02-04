5cec455168441ab7cbb3a229c79e2819c27fe6b0
Tia Firing <tia.firing@nav.no>
2026-02-04 07:46:30 +0100

    1869 legger til Team Tiltak som mulig kilde

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/tiltaksdeltakelse/infra/repo/TiltakskildeDb.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/tiltaksdeltakelse/infra/repo/TiltakskildeDb.kt
@@ -5,16 +5,19 @@ import no.nav.tiltakspenger.saksbehandling.tiltaksdeltakelse.Tiltakskilde
 enum class TiltakskildeDb {
     Arena,
     Komet,
+    TeamTiltak,
 }
 
 internal fun String.toTiltakskilde(): Tiltakskilde =
     when (TiltakskildeDb.valueOf(this)) {
         TiltakskildeDb.Arena -> Tiltakskilde.Arena
         TiltakskildeDb.Komet -> Tiltakskilde.Komet
+        TiltakskildeDb.TeamTiltak -> Tiltakskilde.TeamTiltak
     }
 
 internal fun Tiltakskilde.toDb(): String =
     when (this) {
         Tiltakskilde.Arena -> TiltakskildeDb.Arena
         Tiltakskilde.Komet -> TiltakskildeDb.Komet
+        Tiltakskilde.TeamTiltak -> TiltakskildeDb.TeamTiltak
     }.toString()
