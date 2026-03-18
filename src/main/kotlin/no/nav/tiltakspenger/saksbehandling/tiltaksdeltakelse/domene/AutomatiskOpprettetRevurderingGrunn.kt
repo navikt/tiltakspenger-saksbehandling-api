2477e528ebc0dd947c5d01615252daf4c76a9ef5
Anders Nomerstad <anders@nomerstad.com>
2026-03-18 18:37:45 +0100

    Kjører jobb for tiltaksdeltaker endring lokalt

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/tiltaksdeltakelse/domene/AutomatiskOpprettetRevurderingGrunn.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/tiltaksdeltakelse/domene/AutomatiskOpprettetRevurderingGrunn.kt
@@ -2,6 +2,9 @@ package no.nav.tiltakspenger.saksbehandling.tiltaksdeltakelse.domene
 
 import no.nav.tiltakspenger.saksbehandling.tiltaksdeltakelse.infra.kafka.jobb.TiltaksdeltakerEndringer
 
+/**
+ *  [hendelseId] id for kafka-hendelsen med endringene som utløste opprettelsen av revurderingen
+ */
 data class AutomatiskOpprettetRevurderingGrunn(
     val endringer: TiltaksdeltakerEndringer,
     val hendelseId: String,
