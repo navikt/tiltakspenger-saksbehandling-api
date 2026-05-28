2a93681d341c0601bee68bc4ad6cb02b88c4abd1
John Andre Hestad <john.andre.hestad@nav.no>
2026-05-28 19:16:51 +0200

    Bruk navkontor fra ny klient hvis den gamle ikke returnerer navkontor

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/oppfølgingsenhet/SammenligningVeilarboppfolgingKlient.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/oppfølgingsenhet/SammenligningVeilarboppfolgingKlient.kt
@@ -2,6 +2,7 @@ package no.nav.tiltakspenger.saksbehandling.oppfølgingsenhet
 
 import arrow.core.Either
 import arrow.core.left
+import arrow.core.right
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
@@ -9,18 +10,25 @@ import no.nav.tiltakspenger.libs.common.Fnr
 
 /**
  * Dekoratør som kjører [KontorhistorikkKlient] i parallell med eksisterende [VeilarboppfolgingKlient]
- * og logger eventuelle forskjeller. Resultatet som returneres er fortsatt det fra den eksisterende
- * klienten - vi endrer ikke faktisk oppførsel i denne iterasjonen.
+ * og logger eventuelle forskjeller.
  *
  * Begge klientene kalles alltid - også i prod - slik at vi kan:
  * 1. Lagre rå request/response fra begge tjenestene (via [NavkontorMedMetadata]/[KanIkkeHenteOppfølgingsenhet]).
- * 2. Senere falle tilbake på den nye klienten dersom den gamle ikke gir svar.
+ * 2. Falle tilbake på den nye klienten i de tilfellene den gamle ikke fant noe navkontor
+ *    ([KanIkkeHenteOppfølgingsenhet.ManglerOppfolgingsenhet]).
  *
- * Utfallet av sammenligningen logges alltid - både ved suksess og feil - slik at vi får
- * sporbarhet på om gammel og ny tjeneste gir samme svar.
+ * Valgregel for hvilket svar som returneres:
+ * - Gammel klient OK -> bruk gammel.
+ * - Gammel klient feilet med [KanIkkeHenteOppfølgingsenhet.ManglerOppfolgingsenhet] og ny klient ga
+ *   et brukbart kontor ([Kontorhistorikk.nyesteAktuelleKontor]) -> bruk det fra ny.
+ * - Ellers -> propagér feilen fra gammel klient.
  *
- * [KontorhistorikkKlient] gjør sin egen detaljerte logging (inkludert sikkerlogg). Vi nøyer oss derfor
- * her med å logge utfallet av selve sammenligningen.
+ * I alle tilfeller logges utfallet av sammenligningen og hvilken klient sitt svar som faktisk ble
+ * benyttet, slik at vi har tydelig sporbarhet både i prod og i dev.
+ *
+ * [KontorhistorikkKlient] og [VeilarboppfolgingKlient]-implementasjonene gjør sin egen detaljerte
+ * logging (inkludert sikkerlogg). Vi nøyer oss derfor her med å logge utfallet av sammenligningen
+ * og valget.
  */
 class SammenligningVeilarboppfolgingKlient(
     private val eksisterende: VeilarboppfolgingKlient,
@@ -57,7 +65,7 @@ class SammenligningVeilarboppfolgingKlient(
                     )
                 }.fold(
                     ifLeft = {
-                        logger.warn { "Sammenligning navkontor: ny klient kastet exception. Gammel flyt fortsetter. $loggkontekst" }
+                        logger.warn { "Sammenligning navkontor: ny klient kastet exception. $loggkontekst" }
                         KanIkkeHenteKontorhistorikk.KallFeilet().left()
                     },
                     ifRight = { it },
@@ -76,14 +84,58 @@ class SammenligningVeilarboppfolgingKlient(
 
             loggSammenligning(eksisterendeResultat, nyttResultat, loggkontekst)
 
-            // Forberedelse: hvis den gamle klienten ikke returnerer noe vil vi senere ønske å falle tilbake
-            // på data fra den nye. I dag returnerer vi fortsatt det eksisterende svaret, men beriker det
-            // (og evt. feil) med metadata fra begge kall slik at konsumenten har alt som trengs for å
-            // lagre rådata og hvilken klient som ble brukt.
-            eksisterendeResultat
-                .map { it.copy(kontorhistorikkKall = nyttKall) }
-                .mapLeft { it.medKontorhistorikkKall(nyttKall) }
+            velgResultat(eksisterendeResultat, nyttResultat, nyttKall, loggkontekst)
         }
+    }
+
+    /**
+     * Velger hvilket svar som returneres til konsumenten:
+     * - Gammel OK -> gammel svaret (beriket med kontorhistorikkKall).
+     * - Gammel [KanIkkeHenteOppfølgingsenhet.ManglerOppfolgingsenhet] + ny ga et brukbart kontor -> bruk ny.
+     * - Ellers -> propagér Left fra gammel (beriket med kontorhistorikkKall).
+     *
+     * Hvilken klient som ble brukt logges eksplisitt.
+     */
+    private fun velgResultat(
+        eksisterendeResultat: Either<KanIkkeHenteOppfølgingsenhet, NavkontorMedMetadata>,
+        nyttResultat: Either<KanIkkeHenteKontorhistorikk, KontorhistorikkMedMetadata>,
+        nyttKall: Klientkall?,
+        loggkontekst: String,
+    ): Either<KanIkkeHenteOppfølgingsenhet, NavkontorMedMetadata> {
+        eksisterendeResultat.onRight {
+            logger.info {
+                "Navkontor: bruker svar fra GAMMEL klient (veilarboppfølging). " +
+                    "kontornummer=${it.navkontor.kontornummer}. $loggkontekst"
+            }
+            return it.copy(kontorhistorikkKall = nyttKall).right()
+        }
+
+        val gammelFeil = eksisterendeResultat.leftOrNull()!!
+        val nyttKontor = (gammelFeil as? KanIkkeHenteOppfølgingsenhet.ManglerOppfolgingsenhet)
+            ?.let { nyttResultat.getOrNull()?.kontorhistorikk?.nyesteAktuelleKontor() }
+
+        if (nyttKontor != null) {
+            logger.info {
+                "Navkontor: bruker svar fra NY klient (kontorhistorikk). " +
+                    "Gammel klient fant ingen oppfølgingsenhet, ny klient ga " +
+                    "kontorId=${nyttKontor.kontorId} (type=${nyttKontor.kontorType}). $loggkontekst"
+            }
+            return NavkontorMedMetadata(
+                navkontor = Navkontor(
+                    kontornummer = nyttKontor.kontorId,
+                    kontornavn = nyttKontor.kontorNavn,
+                ),
+                brukteKlient = BruktNavkontorKlient.KONTORHISTORIKK,
+                veilarboppfolgingKall = gammelFeil.veilarboppfolgingKall,
+                kontorhistorikkKall = nyttKall,
+            ).right()
+        }
+
+        logger.info {
+            "Navkontor: bruker svar fra GAMMEL klient (feil propageres). " +
+                "Feil=$gammelFeil. $loggkontekst"
+        }
+        return gammelFeil.medKontorhistorikkKall(nyttKall).left()
     }
 
     private fun loggSammenligning(
--- src/test/kotlin/no/nav/tiltakspenger/saksbehandling/oppfølgingsenhet/SammenligningVeilarboppfolgingKlientTest.kt
+++ src/test/kotlin/no/nav/tiltakspenger/saksbehandling/oppfølgingsenhet/SammenligningVeilarboppfolgingKlientTest.kt
@@ -101,6 +101,83 @@ internal class SammenligningVeilarboppfolgingKlientTest {
     }
 
     @Test
+    fun `faller tilbake på ny klient når gammel klient mangler oppfolgingsenhet`() {
+        val klient = SammenligningVeilarboppfolgingKlient(
+            eksisterende = VeilarboppfolgingFakeKlient {
+                KanIkkeHenteOppfølgingsenhet.ManglerOppfolgingsenhet(
+                    veilarboppfolgingKall = Klientkall(request = "{}", response = "{}", httpStatus = 200),
+                ).left()
+            },
+            kontorhistorikkKlient = KontorhistorikkFakeKlient { fnr ->
+                KontorhistorikkMedMetadata(
+                    kontorhistorikk = Kontorhistorikk(
+                        listOf(
+                            Kontorhistorikkinnslag(
+                                kontorId = "1234",
+                                kontorNavn = "Nav Ny",
+                                kontorType = KontorType.ARBEIDSOPPFOLGING,
+                                endretTidspunkt = LocalDateTime.parse("2024-05-01T10:00:00"),
+                            ),
+                        ),
+                    ),
+                    kall = KontorhistorikkFakeKlient.defaultKall(fnr),
+                ).right()
+            },
+        )
+
+        runTest {
+            val resultat = klient.hentOppfolgingsenhet(Fnr.random()).getOrNull()!!
+            resultat.navkontor shouldBe Navkontor(kontornummer = "1234", kontornavn = "Nav Ny")
+            resultat.brukteKlient shouldBe BruktNavkontorKlient.KONTORHISTORIKK
+            resultat.veilarboppfolgingKall.shouldNotBeNull()
+            resultat.kontorhistorikkKall.shouldNotBeNull()
+        }
+    }
+
+    @Test
+    fun `propagerer ManglerOppfolgingsenhet når både gammel og ny mangler kontor`() {
+        val klient = SammenligningVeilarboppfolgingKlient(
+            eksisterende = VeilarboppfolgingFakeKlient {
+                KanIkkeHenteOppfølgingsenhet.ManglerOppfolgingsenhet(
+                    veilarboppfolgingKall = Klientkall(request = "{}", response = "{}", httpStatus = 200),
+                ).left()
+            },
+            kontorhistorikkKlient = KontorhistorikkFakeKlient { fnr ->
+                KontorhistorikkMedMetadata(
+                    kontorhistorikk = Kontorhistorikk(emptyList()),
+                    kall = KontorhistorikkFakeKlient.defaultKall(fnr),
+                ).right()
+            },
+        )
+
+        runTest {
+            val feil = klient.hentOppfolgingsenhet(Fnr.random()).leftOrNull()!!
+            feil.shouldBeInstanceOf<KanIkkeHenteOppfølgingsenhet.ManglerOppfolgingsenhet>()
+            feil.kontorhistorikkKall.shouldNotBeNull()
+        }
+    }
+
+    @Test
+    fun `andre Left enn ManglerOppfolgingsenhet faller IKKE tilbake på ny klient`() {
+        // Vi vil ikke skjule kall-/HTTP-/tjenestefeil ved å bytte til ny klient. Bare nullsvar (mangler).
+        val klient = SammenligningVeilarboppfolgingKlient(
+            eksisterende = VeilarboppfolgingFakeKlient {
+                KanIkkeHenteOppfølgingsenhet.UventetHttpStatus(
+                    status = 503,
+                    veilarboppfolgingKall = Klientkall(request = "{}", response = "fail", httpStatus = 503),
+                ).left()
+            },
+            kontorhistorikkKlient = KontorhistorikkFakeKlient(),
+        )
+
+        runTest {
+            val feil = klient.hentOppfolgingsenhet(Fnr.random()).leftOrNull()!!
+            feil.shouldBeInstanceOf<KanIkkeHenteOppfølgingsenhet.UventetHttpStatus>()
+            feil.kontorhistorikkKall.shouldNotBeNull()
+        }
+    }
+
+    @Test
     fun `flere historikkinnslag - returnerer fortsatt eksisterende svar uavhengig`() {
         // Sanity-sjekk: dekoratøren skal håndtere flere innslag uten å påvirke svaret.
         val klient = SammenligningVeilarboppfolgingKlient(
