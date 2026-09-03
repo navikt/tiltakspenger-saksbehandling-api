e7e60d34bdd4a0daa79902f9c893aa5c51a93445
Ole-Sander Halla Johansen <Ole-Sander.Halla.Johansen@nav.no>
2026-09-03 15:17:11 +0200

    ferdigstilt route og service sånn at saksbehandlere kan ta flere saker

--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/infra/route/TaFlereRammebehandlingRoute.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/infra/route/TaFlereRammebehandlingRoute.kt
@@ -4,12 +4,16 @@ import io.ktor.server.auth.principal
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import no.nav.tiltakspenger.libs.common.RammebehandlingId
+import no.nav.tiltakspenger.libs.common.SakId
+import no.nav.tiltakspenger.libs.common.Saksbehandler
 import no.nav.tiltakspenger.libs.ktor.common.withBody
 import no.nav.tiltakspenger.libs.texas.TexasPrincipalInternal
 import no.nav.tiltakspenger.libs.texas.saksbehandler
 import no.nav.tiltakspenger.saksbehandling.auditlog.AuditService
 import no.nav.tiltakspenger.saksbehandling.auth.tilgangskontroll.TilgangskontrollService
+import no.nav.tiltakspenger.saksbehandling.behandling.service.behandling.RammebehandlingMedSakId
 import no.nav.tiltakspenger.saksbehandling.behandling.service.behandling.TaRammebehandlingService
+import no.nav.tiltakspenger.saksbehandling.behandling.service.behandling.TaRammebehandlingerKommando
 import no.nav.tiltakspenger.saksbehandling.felles.autoriserteBrukerroller
 import no.nav.tiltakspenger.saksbehandling.felles.krevSaksbehandlerEllerBeslutterRolle
 import no.nav.tiltakspenger.saksbehandling.infra.route.correlationId
@@ -17,9 +21,26 @@ import java.time.Clock
 
 private const val TA_FLERE_BEHANDLING_PATH = "/behandlinger/ta"
 
-data class TaFlereRammebehandlinger(
-    val behandlingIder: List<String>,
-)
+private data class TaFlereRammebehandlinger(
+    val behandlinger: List<BehandlingMedSakIdBody>,
+) {
+    fun tilKommando(saksbehandler: Saksbehandler): TaRammebehandlingerKommando {
+        return TaRammebehandlingerKommando(
+            saksbehandler = saksbehandler,
+            behandlinger = behandlinger.map {
+                RammebehandlingMedSakId(
+                    behandlingId = RammebehandlingId.fromString(it.behandlingId),
+                    sakId = SakId.fromString(it.sakId),
+                )
+            },
+        )
+    }
+
+    data class BehandlingMedSakIdBody(
+        val behandlingId: String,
+        val sakId: String,
+    )
+}
 
 fun Route.taFlereRammebehandlingerRoute(
     auditService: AuditService,
@@ -32,18 +53,14 @@ fun Route.taFlereRammebehandlingerRoute(
         logger.debug { "Mottatt post-request på '$TA_FLERE_BEHANDLING_PATH' - Knytter saksbehandler/beslutter til flere behandlinger." }
         val token = call.principal<TexasPrincipalInternal>()?.token ?: return@post
         val saksbehandler = call.saksbehandler(autoriserteBrukerroller()) ?: return@post
-        call.withBody<TaFlereRammebehandlinger> { behandlingIder ->
+        call.withBody<TaFlereRammebehandlinger> { body ->
 
-            val behandlingIder = behandlingIder.behandlingIder.map {
-                RammebehandlingId.fromString(it)
-            }
+            val kommando = body.tilKommando(saksbehandler)
 
             val correlationId = call.correlationId()
             krevSaksbehandlerEllerBeslutterRolle(saksbehandler)
 
-            taBehandlingService.hentSakIder(behandlingIder = behandlingIder)
-
-            tilgangskontrollService.harTilgangTilPersonForSakId(sakId, saksbehandler, token)
+            kommando.behandlinger.distinctBy { it.sakId }.forEach { tilgangskontrollService.harTilgangTilPersonForSakId(it.sakId, saksbehandler, token) }
         }
     }
 }
--- src/main/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/service/behandling/TaRammebehandlingService.kt
+++ src/main/kotlin/no/nav/tiltakspenger/saksbehandling/behandling/service/behandling/TaRammebehandlingService.kt
@@ -1,6 +1,8 @@
 package no.nav.tiltakspenger.saksbehandling.behandling.service.behandling
 
 import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.tiltakspenger.libs.common.RammebehandlingId
 import no.nav.tiltakspenger.libs.common.SakId
@@ -12,8 +14,10 @@ import no.nav.tiltakspenger.saksbehandling.behandling.domene.Rammebehandlingssta
 import no.nav.tiltakspenger.saksbehandling.behandling.domene.Rammebehandlingsstatus.UNDER_BESLUTNING
 import no.nav.tiltakspenger.saksbehandling.behandling.domene.ta.KunneIkkeTaBehandling
 import no.nav.tiltakspenger.saksbehandling.behandling.domene.ta.taBehandling
+import no.nav.tiltakspenger.saksbehandling.felles.getOrThrow
 import no.nav.tiltakspenger.saksbehandling.sak.Sak
 import no.nav.tiltakspenger.saksbehandling.statistikk.StatistikkService
+import no.nav.tiltakspenger.saksbehandling.statistikk.saksstatistikk.StatistikkDTO
 import java.time.Clock
 
 class TaRammebehandlingService(
@@ -49,15 +53,48 @@ class TaRammebehandlingService(
         }
     }
 
-    suspend fun hentSakIder(
-        behandlingIder: List<RammebehandlingId>,
-    ): Either<KunneIkkeTaBehandling, List<SakId>> {
-        // Spørsmål Anders - er det nødvendig når det bare er en read?
-        // Hvordan håndtere feil - f.eks. hvis en behandlingsId ikke gir en sak?
+    suspend fun taFlereBehandlinger(
+        kommando: TaRammebehandlingerKommando,
+    ): Either<KunneIkkeTaBehandling, Unit> { // Husk å oppdater unit for det frontend trenger
+
+        val behandlingerMedStatistikk: List<Pair<Rammebehandling, StatistikkDTO>> = kommando.behandlinger.map {
+            val (sak, behandling) = behandlingService.hentSakOgRammebehandling(it.sakId, it.behandlingId)
+
+            val pair = behandling.taBehandling(kommando.saksbehandler, clock).onLeft {
+                return@taFlereBehandlinger it.left()
+            }.map { (oppdatertRammebehandling, statistikkhendelser) ->
+                // Verifiser at saken er ok
+                sak.oppdaterRammebehandling(oppdatertRammebehandling)
+                val statistikkDTO = statistikkService.generer(statistikkhendelser)
+
+                oppdatertRammebehandling to statistikkDTO
+            }
+
+            pair.getOrThrow()
+        }
+
         sessionFactory.withTransactionContext { tx ->
-            return behandlingIder.map { behandlingId ->
-                rammebehandlingRepo.hent(behandlingId, tx).sakId
+            behandlingerMedStatistikk.forEach { (rammebehandling, statistikk) ->
+                when (rammebehandling.status) {
+                    UNDER_BEHANDLING -> rammebehandlingRepo.taBehandlingSaksbehandler(rammebehandling, tx)
+                    UNDER_BESLUTNING -> rammebehandlingRepo.taBehandlingBeslutter(rammebehandling, tx)
+                    else -> throw IllegalStateException("Vi havnet i en ugyldig tilstand etter vi tok behandlingen - behandlingId: ${rammebehandling.id}, status: ${rammebehandling.status}")
+                }
+                statistikkService.lagre(statistikk, tx)
             }
         }
+
+        return Unit.right()
     }
 }
+
+data class TaRammebehandlingerKommando(
+    val saksbehandler: Saksbehandler,
+    val behandlinger: List<RammebehandlingMedSakId>,
+
+)
+
+data class RammebehandlingMedSakId(
+    val behandlingId: RammebehandlingId,
+    val sakId: SakId,
+)
